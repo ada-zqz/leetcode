f4b1ef895cf57052ddac5873ba8663e517cc64d6
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-07 16:03:48 +0800

    Update dp198_HouseRobber.cpp
    不用判断前一个点有没有被偷

--- dp198_HouseRobber.cpp
+++ dp198_HouseRobber.cpp
@@ -1,17 +1,16 @@
+# define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 0) {return 0;}
         vector<int> dp(n);
-        int pre;
         
+        if(n == 1) {return nums[0];}
         dp[0] = nums[0];
-        pre = 0;
-        if(n == 1) {return dp[0];}
         if(nums[1] > nums[0]) {
             dp[1] = nums[1];
-            pre = 1;
         }
         else {
             dp[1] = dp[0];
@@ -19,19 +18,7 @@ public:
         if(n == 2) {return dp[1];}
         
         for(int i = 2; i < nums.size(); i++) {
-            if(pre == i - 1) {
-                if(dp[i - 2] + nums[i] > dp[i - 1]) {
-                    dp[i] = dp[i - 2] + nums[i];
-                    pre = i;
-                }
-                else {
-                    dp[i] = dp[i - 1];
-                }
-            }
-            else {
-                dp[i] = dp[i - 1] + nums[i];
-                pre = i;
-            }
+            dp[i] = MAX(dp[i - 2] + nums[i], dp[i - 1]);
         }
         
         return dp[nums.size() - 1];
