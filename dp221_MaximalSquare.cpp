f37886eaff9d6628c61d4f3e2abfb7bc0a310c2a
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-07 20:12:31 +0800

    Update dp221_MaximalSquare.cpp
    第20行，(matrix[i][j] - '0')直接用1代替

--- dp221_MaximalSquare.cpp
+++ dp221_MaximalSquare.cpp
@@ -17,7 +17,7 @@ public:
                     dp[i][j] = matrix[i][j] - '0';
                 }
                 else {
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + (matrix[i][j] - '0');
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1; //(matrix[i][j] - '0')
                 }
                 if(dp[i][j] > maxs) {
                         maxs = dp[i][j];
