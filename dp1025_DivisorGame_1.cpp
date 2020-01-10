099597e54081ec8394018fa7a2ad3500ac77dc05
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-11 00:34:09 +0800

    Create dp1025_DivisorGame_1.cpp
    数学简化

---
+++ dp1025_DivisorGame_1.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool divisorGame(int N) {
+        // 可能借助1,2,3,4,5...理解
+        // 偶数A能赢，减1变成奇数给B就行；奇数的话，减一次（一定时奇数）变成偶数，A只能输
+        if(N % 2 == 0) {return true;}
+        else {return false;}
+    }
+};
