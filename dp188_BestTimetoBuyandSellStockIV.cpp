4b965a3d3b7284614081243a1680b42f20a2513c
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-10 12:36:17 +0800

    空间O(kn)变成O(2n)

--- dp188_BestTimetoBuyandSellStockIV.cpp
+++ dp188_BestTimetoBuyandSellStockIV.cpp
@@ -19,20 +19,21 @@ public:
             return maxp;
         }
         
-        vector<vector<int>> dp(k + 1, vector<int>(n, 0));
+        vector<int> dp1(n, 0), dp2(n, 0);
         int premax;
         for(int i = 1; i < k + 1; i++) {
-            premax = dp[i - 1][0] - prices[0];
+            premax = dp1[0] - prices[0];
             for(int j = 1; j < n; j++) {
                 // 到j为止，至多i次交易的最大收益 dp[i][j]
                 // MAX(到j-1为止，至多i次交易的最大收益dp[i][j-1]， 第j项被交易的最大值)
                 // max(prices[j] - prices[l] + dp[i - 1][l]), 0<=l<=j-1
                 // (l没被交易，prices相减符合逻辑；l被交易了，再交易j相当于l没被交易，符合收益最大)
                 // prices[j] - max(dp[i-1][l] - prices[l])
-                dp[i][j] = MAX(dp[i][j - 1], prices[j] + premax);
-                premax = MAX(premax, dp[i - 1][j] - prices[j]);
+                dp2[j] = MAX(dp2[j - 1], prices[j] + premax);
+                premax = MAX(premax, dp1[j] - prices[j]);
             }
+            swap(dp1, dp2);
         }
-        return dp[k][n - 1];
+        return dp1[n - 1];
     }
 };
