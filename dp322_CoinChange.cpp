74732455c3aed0eabddf029e1206ec8fccaf0ad3
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-21 12:36:15 +0800

    Bfs 但是内存超出
    [1,2,5]
    100
    Memory Limit Exceeded

--- dp322_CoinChange.cpp
+++ dp322_CoinChange.cpp
@@ -1,3 +1,9 @@
+struct Part{
+    int step; //第几步
+    int amount; //还需要补足的amount
+    Part(int s, int na): step(s), amount(na) {}
+};
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -6,15 +12,29 @@ public:
         if(amount == 0)
             return 0;
         
-        vector<int> dp(amount + 1, 1<<30);
-        dp[0] = 0;
-        for(int i = 1; i < amount + 1; i++) {
-            for(int j = 0; j < coins.size(); j++) {
-                if(i >= coins[j]) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
+        queue<Part> q;
+        for(int i = 0; i < coins.size(); i++) {
+            if(amount == coins[i]) {
+                return 1;
+            }
+            if(amount > coins[i]) {
+                q.push(Part(1, amount - coins[i]));
             }
         }
-        return (dp[amount]) < (1<<30) ? dp[amount] : -1;
+        while(!q.empty()) {
+            Part p = q.front();
+            if(p.amount == 0) {
+                return p.step;
+            }
+            else {
+                for(int i = 0; i < coins.size(); i++) {
+                    if(coins[i] <= p.amount) {
+                        q.push(Part(p.step + 1, p.amount - coins[i]));
+                    }
+                }
+            }
+            q.pop();
+        }
+        return -1;
     }
 };
