407f879f8a6000987fea1258ec71a499b5fa34c7
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-22 19:42:51 +0800

    换一种思路，类似最长公共子序列
    但是速度还是很慢

--- dp516_LongestPalindromicSubsequence.cpp
+++ dp516_LongestPalindromicSubsequence.cpp
@@ -3,38 +3,25 @@ public:
     int longestPalindromeSubseq(string s) {
         if(s.size() == 1)
             return 1;
-        vector<int> dp(s.size(), 1);   //dp[i]存放到i为止，最长的回文长度
-        vector<int> pres(s.size(), 0); //pres[i]存放到i为止，最长回文的第一个字母的位置
-        for(int i = 1; i < s.size(); i++) { //先处理字母相等的回文，每次只加1
-            pres[i] = i;
-            for(int j = i - 1; j >= 0; j--) {
-                if(s[i] == s[j]) {
-                    dp[i] = dp[j] + 1;
-                    pres[i] = pres[j];
-                    break;
-                }                
-            }
-        }
+        //dp[i][j]存放以s[i]结尾，以s[j]开始的最长的回文长度
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 1));   
+        
         for(int i = 1; i < s.size(); i++) {
-            for(int j = i - 1; j >= 0; j--) { //根据i之前的回文推断i出的回文
-                // cout<<i<<j<<dp[i]<<pres[i]<<dp[j]<<pres[j]<<endl;
-                if(dp[j] > dp[i]) {
-                    dp[i] = dp[j];
-                    pres[i] = pres[j];
-                }
-                if(pres[j] > 0) { //if(pres[j] == 0) s[i]加不上去
-                    for(int k = pres[j] - 1; k >= 0; k--) {
-                        if(s[k] == s[i]) {
-                            if(dp[j] + 2 > dp[i]) {
-                                dp[i] = dp[j] + 2;
-                                pres[i] = k;
-                                break;
-                            }
-                        }
+            for(int j = i - 1; j >= 0; j--) {
+                if((i - j) == 1) {
+                    if(s[i] == s[j]) {
+                        dp[i][j] = 2;
                     }
+                    continue;
+                }
+                if(s[i] == s[j]) { //i - j > 1
+                    dp[i][j] = dp[i - 1][j + 1] + 2;
+                }
+                else { //i > j + 1
+                    dp[i][j] = max(dp[i][j + 1], dp[i - 1][j]);
                 }
             }
         }
-        return dp[s.size() - 1];
-    }
+        return dp[s.size() - 1][0];
+    }//类似最长公共子序列
 };
