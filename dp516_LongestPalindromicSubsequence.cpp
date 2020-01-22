3c3005b12144d9708618119c9145cbb0d68a3109
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-22 19:58:24 +0800

    leetcode判断的时间和空间排名都升高

--- dp516_LongestPalindromicSubsequence.cpp
+++ dp516_LongestPalindromicSubsequence.cpp
@@ -4,20 +4,15 @@ public:
         if(s.size() == 1)
             return 1;
         //dp[i][j]存放以s[i]结尾，以s[j]开始的最长的回文长度
-        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 1));   
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));   
         
         for(int i = 1; i < s.size(); i++) {
+            dp[i][i] = 1;
             for(int j = i - 1; j >= 0; j--) {
-                if((i - j) == 1) {
-                    if(s[i] == s[j]) {
-                        dp[i][j] = 2;
-                    }
-                    continue;
-                }
                 if(s[i] == s[j]) { //i - j > 1
                     dp[i][j] = dp[i - 1][j + 1] + 2;
                 }
-                else { //i > j + 1
+                else { 
                     dp[i][j] = max(dp[i][j + 1], dp[i - 1][j]);
                 }
             }
