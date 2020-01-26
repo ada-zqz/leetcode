83b62b939bf3d3d2ae5ed1806545a5fa05df517e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-26 13:05:07 +0800

    换成Dfs的思路，但是超时

--- dp688_KnightProbabilityinChessboard.cpp
+++ dp688_KnightProbabilityinChessboard.cpp
@@ -1,36 +1,22 @@
-struct DIR {
-    int h, v;
-    DIR(int horizon, int vertical): h(horizon), v(vertical) {}
-};
-
 class Solution {
 public:
     double knightProbability(int N, int K, int r, int c) {
         if(K == 0)
             return 1;
-        float dp[101][25][25];  //dp[k][i][j]存储从(i, j)走k步存活的概率
-        memset(dp, 0, sizeof(dp));
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                dp[0][i][j] = 1;
-            }
-        }
-        // dir存储可以走的8个方向
-        DIR dir[8] = {DIR(1, 2), DIR(2, 1), DIR(2, -1), DIR(1, -2),
-                      DIR(-1, -2), DIR(-2, -1), DIR(-2, 1), DIR(-1, 2)};
         
-        for(int nowK = 1; nowK <= K; nowK++) {
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    for(int l = 0; l < 8; l++) {
-                        int ip = i + dir[l].h, jp = j + dir[l].v;
-                        if(ip >= 0 && ip < N && jp >= 0 && jp < N) {
-                            dp[nowK][i][j] +=  (1./8) * dp[nowK - 1][ip][jp];
-                        }
-                    }
-                }
-            }
+        return prob(N, K, r, c);
+    }
+    
+    float prob(int& N, int K, int r, int c) {
+        if(r < 0 || r >= N || c < 0 || c >= N) {
+            return 0;
         }
-        return dp[K][r][c];
+        if(K == 0) {
+            return 1;
+        }
+        float pb = 0;
+        K -= 1;
+        pb = 0.125 * (prob(N, K, r - 1, c + 2) + prob(N, K, r - 1, c - 2) + prob(N, K, r - 2, c + 1) + prob(N, K, r - 2, c - 1) + prob(N, K, r + 1, c + 2) + prob(N, K, r + 1, c - 2) + prob(N, K, r + 2, c + 1) + prob(N, K, r + 2, c - 1));
+        return pb;
     }
 };
