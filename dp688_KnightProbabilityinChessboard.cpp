8693dc7840f4d5ee18a462fa4e7a25b2fb6c994c
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-26 13:24:20 +0800

    1/8最后在统一做，减少计算时间
    Runtime: 12 ms, faster than 63.15% of C++ online submissions for Knight Probability in Chessboard.
    Memory Usage: 9.2 MB, less than 100.00% of C++ online submissions for Knight Probability in Chessboard.

--- dp688_KnightProbabilityinChessboard.cpp
+++ dp688_KnightProbabilityinChessboard.cpp
@@ -3,22 +3,22 @@ public:
     double knightProbability(int N, int K, int r, int c) {
         if(K == 0)
             return 1;
-        float dp[101][25][25];
+        double dp[101][25][25];
         memset(dp, 0, sizeof(dp));
-        return prob(N, K, r, c, dp);
+        return prob(N, K, r, c, dp) / pow(8, K);
     }
     
-    float prob(int& N, int K, int r, int c, float (&dp)[101][25][25]) {
+    double prob(int& N, int K, int r, int c, double (&dp)[101][25][25]) {
         if(r < 0 || r >= N || c < 0 || c >= N) {
-            return 0;
+            return 0.;
         }
         if(K == 0) {
-            return 1;
+            return 1.;
         }
         if(dp[K][r][c] != 0) {
             return dp[K][r][c];
         }
-        dp[K][r][c] =  0.125 * (prob(N, K - 1, r - 1, c + 2, dp) + prob(N, K - 1, r - 1, c - 2, dp) + prob(N, K - 1, r - 2, c + 1, dp) + prob(N, K - 1, r - 2, c - 1, dp) + prob(N, K - 1, r + 1, c + 2, dp) + prob(N, K - 1, r + 1, c - 2, dp) + prob(N, K - 1, r + 2, c + 1, dp) + prob(N, K - 1, r + 2, c - 1, dp));
+        dp[K][r][c] =  prob(N, K - 1, r - 1, c + 2, dp) + prob(N, K - 1, r - 1, c - 2, dp) + prob(N, K - 1, r - 2, c + 1, dp) + prob(N, K - 1, r - 2, c - 1, dp) + prob(N, K - 1, r + 1, c + 2, dp) + prob(N, K - 1, r + 1, c - 2, dp) + prob(N, K - 1, r + 2, c + 1, dp) + prob(N, K - 1, r + 2, c - 1, dp);
         return dp[K][r][c];
     }
 };
