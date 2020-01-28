554fc7ab4295df987391453c9d770f458a9986cd
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-01-28 18:24:36 +0800

    Update dp1147_LongestChunkedPalindromeDecomposition.cpp
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Longest Chunked Palindrome Decomposition.
    Memory Usage: 11.5 MB, less than 100.00% of C++ online submissions for Longest Chunked Palindrome Decomposition.

--- dp1147_LongestChunkedPalindromeDecomposition.cpp
+++ dp1147_LongestChunkedPalindromeDecomposition.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int longestDecomposition(string text) {
-        if(text.size() == 1)
-            return 1;
         int k = 1, n = text.size();
-        for(int i = 0; i < n/2;) {
-            if(text[i] == text[n - i - 1]) {
-                cout << i << " " << text[i] << " " << text[n - i - 1] << endl;
-                k += 2;
-                i++;
-                continue;
-            }
-            for(int j = 2; j <= n/2 - i; j++) {
-                if(text.substr(i, j) == text.substr(n - i - j, j)) {
-                    k += 2;
-                    cout << i << " " << j << " " << k << " " << text.substr(i, j) << " " << text.substr(n - i - j, j) << endl;
-                    i = i + j;
-                }
+        for(int i = 0; i < n/2; i++) {
+            if(text.substr(0, i + 1) == text.substr(n - i - 1, i + 1)) {
+                // cout << i << " " << text.substr(0, i + 1) << " " << text.substr(n - i - 1, i + 1) << endl;
+                return 2 + longestDecomposition(text.substr(i + 1, n - 2 * i - 2));
             }
         }
-        return k;
+        return (n == 0) ? 0 : 1;
     }
 };
