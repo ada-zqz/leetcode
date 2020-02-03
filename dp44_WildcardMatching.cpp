2d801b0e3c4f3cca8ed5b064d5d48bfe57e85ccf
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-03 18:51:46 +0800

    第四版代码，贪心
    Runtime: 8 ms, faster than 93.41% of C++ online submissions for Wildcard Matching.
    Memory Usage: 8.9 MB, less than 73.08% of C++ online submissions for Wildcard Matching.

--- dp44_WildcardMatching.cpp
+++ dp44_WildcardMatching.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        if(m == 0 && n > 0)
-            return false;
-        vector<bool> cur(m + 1, false); 
-        bool pre, temp;  //dp[i - 1][j - 1]
-        cur[0] = true;
+        int i = 0, j = 0, smatch_pos, ptomatch_pos = -1;
         
-        for(int i = 0; i <= n; i++) {
-            pre = (i <= 1) ? true : false;
-            for(int j = 1; j <= m; j++) {
-                temp = cur[j];  //没有更新前的值，属于dp[i-1][j]
-                if(p[j - 1] == '*') {
-                    // '*'匹配0个字符 or 匹配当前字符 or 匹配多个字符
-                    cur[j] = cur[j - 1] || cur[j];  // 等式后的cur[j]是i-1对应的值，更新cur
-                }
-                else if(i > 0 && pre && (s[i - 1] == p[j - 1] || p[j - 1] == '?')) {
-                    cur[j] = true;
-                }
-                else {
-                    cur[j] = false;  //为false也要赋值，不然会出错
-                }
-                pre = temp;  //pre由代表dp[i-1][j-1]变为dp[i-1][j]，之后j++
+        while(i < n) {
+            if(j < m && (s[i] == p[j] || p[j] == '?')) {
+                // j<n是类似出现abcd,ab的情况
+                i++;
+                j++;
             }
+            else if(j < m && p[j] == '*') {
+                //smathch_pos可以用*匹配
+                //但是先不匹配，而是用p[j+1]看是否匹配s[i]，匹配不成功再换p[j+1]匹配s[i+1]
+                smatch_pos = i; 
+                ptomatch_pos = j++;
+            }
+            else if(ptomatch_pos >= 0) {
+                //匹配不成功再换'*'后的p[j+1]匹配s[i+1]
+                i = ++smatch_pos;
+                j = ptomatch_pos + 1;
+            }
+            else return false;
         }
-        return cur[m];
+        while(j < m && p[j] == '*') j++;
+        return j == m;
     }
 };
