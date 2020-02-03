80a208e68430e8db9e6ff571d8cc322dd1a2da01
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-03 15:23:37 +0800

    空间再缩小，但是时间变长
    Runtime: 80 ms, faster than 43.49% of C++ online submissions for Wildcard Matching.
    Memory Usage: 8.9 MB, less than 73.08% of C++ online submissions for Wildcard Matching.

--- dp44_WildcardMatching.cpp
+++ dp44_WildcardMatching.cpp
@@ -2,23 +2,29 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<bool> pre(m + 1, false), cur(m + 1, false); 
+        if(m == 0 && n > 0)
+            return false;
+        vector<bool> cur(m + 1, false); 
+        bool pre, temp;  //dp[i - 1][j - 1]
         cur[0] = true;
         
         for(int i = 0; i <= n; i++) {
+            pre = (i <= 1) ? true : false;
             for(int j = 1; j <= m; j++) {
+                temp = cur[j];  //没有更新前的值，属于dp[i-1][j]
                 if(p[j - 1] == '*') {
                     // '*'匹配0个字符 or 匹配当前字符 or 匹配多个字符
-                    cur[j] = cur[j - 1] || pre[j]; //pre[j - 1]匹配上，p[j]一定也能匹配
+                    cur[j] = cur[j - 1] || cur[j];  // 等式后的cur[j]是i-1对应的值，更新cur
                 }
-                else if(pre[j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '?')) {
+                else if(i > 0 && pre && (s[i - 1] == p[j - 1] || p[j - 1] == '?')) {
                     cur[j] = true;
                 }
+                else {
+                    cur[j] = false;  //为false也要赋值，不然会出错
+                }
+                pre = temp;  //pre由代表dp[i-1][j-1]变为dp[i-1][j]，之后j++
             }
-            cur.swap(pre);
-            cur.assign(cur.size(), false);
-            // cout << i << s[i - 1] << pre[i] << endl;
         }
-        return pre[m];
+        return cur[m];
     }
 };
