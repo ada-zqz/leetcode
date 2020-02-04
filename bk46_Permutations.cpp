3dec071119acfaf23cfd170aa0bbf1b8d3365d2e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-04 09:20:43 +0800

    dfs中swap(nums[begin], nums[i]);
    Runtime: 8 ms, faster than 98.89% of C++ online submissions for Permutations.
    Memory Usage: 9.2 MB, less than 95.52% of C++ online submissions for Permutations.

--- bk46_Permutations.cpp
+++ bk46_Permutations.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<bool> used(nums.size(), 0); //nums[i]是否已经使用过
-        vector<int> pm;
         
-        dfs(pm, used, nums, res);
+        dfs(0, nums, res);
         return res;
     }
-    void dfs(vector<int> pm, vector<bool> used, vector<int>& nums, vector<vector<int>>& res) {
-        if(pm.size() == nums.size()) {
-            res.push_back(pm);
+    void dfs(int begin, vector<int>& nums, vector<vector<int>>& res) {
+        if(begin == nums.size()) {
+            res.push_back(nums);
             return;
         }
-        for(int i = 0; i < nums.size(); i++) {
-            if(!used[i]) {
-                pm.push_back(nums[i]);
-                used[i] = true;
-                dfs(pm, used, nums, res);
-                pm.pop_back();
-                used[i] = false;
-            }
+        for(int i = begin; i < nums.size(); i++) {
+            swap(nums[begin], nums[i]);
+            dfs(begin + 1, nums, res);
+            swap(nums[begin], nums[i]);
         }
     }
 };
