41d90da298d8fcd7b0b8aa4583eac0cef6339879
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-09 14:44:54 +0800

    Update dp1143_LongestCommonSubsequence.cpp
    Runtime: 720 ms, faster than 22.89% of Python3 online submissions for Longest Common Subsequence.
    Memory Usage: 35.7 MB, less than 100.00% of Python3 online submissions for Longest Common Subsequence.

--- dp1143_LongestCommonSubsequence.cpp
+++ dp1143_LongestCommonSubsequence.cpp
@@ -3,13 +3,10 @@ import numpy as np
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp = np.zeros((len(text1) + 1, len(text2) + 1))
-        res = 0
         for n1, c1 in enumerate(text1):
             for n2, c2 in enumerate(text2):
                 if c1 == c2:
                     dp[n1 + 1, n2 + 1] = dp[n1, n2] + 1
                 else:
                     dp[n1 + 1, n2 + 1] = max(dp[n1, n2 + 1], dp[n1 + 1, n2])
-                # print(n1, ' ', n2, ' ', res, ' ', dp[n1 + 1, n2 + 1])
-                res = max(res, dp[n1 + 1, n2 + 1])
-        return int(res)
+        return int(dp[n1 + 1, n2 + 1])
