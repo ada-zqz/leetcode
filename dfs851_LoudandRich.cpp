4e059f14ab648c59a3226ba96477cb3c3bc7281a
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-15 18:13:53 +0800

    dfs
    Runtime: 112 ms, faster than 40.72% of C++ online submissions for Loud and Rich.
    Memory Usage: 36 MB, less than 100.00% of C++ online submissions for Loud and Rich.

--- dfs851_LoudandRich.cpp
+++ dfs851_LoudandRich.cpp
@@ -2,54 +2,30 @@ class Solution {
 public:
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
         int n = quiet.size();
+        vector<int> ans(n, -1);
+        if(richer.empty()) {
+            for(int i = 0; i < n; i++) ans[i] = i;
+            return ans;
+        }
+        
         // all people who definitely have equal to or more money than person i
-        vector<vector<int>> rclist(n);
+        vector<vector<int>> larger(n);
+        for(auto v: richer) larger[v[1]].push_back(v[0]);
         
-        // find all people who definitely have equal to or more money than person i
-        for(auto v: richer) {
-            // [0]: [1]; [1]: [2, 3], ...
-            rclist[v[1]].push_back(v[0]);
-        }
         for(int i = 0; i < n; i++) {
-            if(!rclist[i].empty()) {
-                vector<bool> added(n, false);
-                queue<int> larger;
-                for(int j: rclist[i]) {
-                    larger.push(j);
-                    added[j] = true;
-                }
-                replenish(larger, rclist[i], rclist, added);
-            }
-        }
-        
-        // find the least quiet person
-        vector<int> ans(n);
-        for(int i = 0; i < n; i++) {
-            int m = quiet[i];
-            ans[i] = i;
-            if(!rclist[i].empty()) {
-                for(int j: rclist[i]) {
-                    if(quiet[j] < m) {
-                        ans[i] = j;
-                        m = quiet[j];
-                    }
-                }
-            }
+            dfs(i, ans, quiet, larger);
         }
         return ans;
     }
-    
-    void replenish(queue<int> larger, vector<int>& append, vector<vector<int>>& rclist, vector<bool>& added) {
-        while(!larger.empty()) {
-            int i = larger.front();
-            larger.pop();
-            for(int j: rclist[i]) {
-                if(!added[j]) {
-                    append.push_back(j);
-                    added[j] = true;
-                }
-                larger.push(j);
+    int dfs(int i, vector<int>& ans, vector<int>& quiet, vector<vector<int>>& larger) {
+        //返回的是比i钱多（或者自己）的人中most quiet的人
+        if(ans[i] >= 0) return ans[i];
+        ans[i] = i;
+        for(int j: larger[i]) { //j比i有钱
+            if(quiet[dfs(j, ans, quiet, larger)] < quiet[ans[i]]) {
+                ans[i] = ans[j];
             }
         }
+        return ans[i];
     }
 };
