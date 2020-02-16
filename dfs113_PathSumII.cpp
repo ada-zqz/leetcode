e26f5efc1a708d0423b4d248f1acdd8cf734f9d3
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-16 08:47:17 +0800

    dfs思路相同，写简洁了，同时空间和时间改进
    Runtime: 12 ms, faster than 88.26% of C++ online submissions for Path Sum II.
    Memory Usage: 19.7 MB, less than 92.11% of C++ online submissions for Path Sum II.

--- dfs113_PathSumII.cpp
+++ dfs113_PathSumII.cpp
@@ -11,29 +11,19 @@ class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
         vector<vector<int>> res;
-        if(root == NULL) return res;
         vector<int> v;
         pathsub(root, sum, v, res);
         return res; 
     }
     void pathsub(TreeNode* root, int sum, vector<int>& v, vector<vector<int>>& res) {
+        if(root == NULL) return;
+        v.push_back(root->val);
         if(sum == root->val && !root->left && !root->right) {
             // 找到叶子节点
-            v.push_back(root->val);
             res.push_back(v);
-            v.erase(v.end() - 1);
-            return;
         }
-        if(root->left) {
-            v.push_back(root->val);
-            pathsub(root->left, sum - root->val, v, res);
-            v.erase(v.end() - 1);
-        }
-        if(root->right) {
-            v.push_back(root->val);
-            pathsub(root->right, sum - root->val, v, res);
-            v.erase(v.end() - 1);
-        }
-        return;
+        pathsub(root->left, sum - root->val, v, res);
+        pathsub(root->right, sum - root->val, v, res);
+        v.pop_back();
     }
 };
