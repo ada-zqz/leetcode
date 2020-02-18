aace85ef83fc356af23281d726631fbd9e843f80
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-18 11:08:29 +0800

    利用set，找不大于k的最接近的数
    Runtime: 400 ms, faster than 53.85% of C++ online submissions for Max Sum of Rectangle No Larger Than K.
    Memory Usage: 100.6 MB, less than 41.18% of C++ online submissions for Max Sum of Rectangle No Larger Than K.

--- dp363_MaxSumofRectangleNoLargerThanK.cpp
+++ dp363_MaxSumofRectangleNoLargerThanK.cpp
@@ -2,38 +2,24 @@ class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
         int m = matrix.size(), n = matrix[0].size();
-        int sum = 1<<31, temp;
+        int sum = 1<<31;
         
         for(int L = 0; L < n; L++) {
             vector<int> w(m, 0);
             for(int R = L; R < n; R++) {
                 for(int i = 0; i < m; i++) w[i] += matrix[i][R];
-                for(int up = 0; up < m; up++) {
-                    vector<int> wsub (w.begin() + up, w.end());
-                    temp = findrowmax(wsub, k);
-                    // cout << L << R << " up" << up << " t" << temp << " s" << sum << endl;   
-                    if(temp == k) return k;
-                    if(temp > k) continue;
-                    if(temp >= sum) {
-                        sum = temp;
-                        break;
-                    }
-                                         
+                
+                set<int> sumfrominit; //从w[0]开始的累加数，顺序排列
+                sumfrominit.insert(0);
+                int acc = 0, wmax = 1<<31;
+                for(int v: w) {
+                    acc += v;
+                    set<int>::iterator it = sumfrominit.lower_bound(acc - k);
+                    if(it != sumfrominit.end()) wmax = max(wmax, acc - *it);
+                    sumfrominit.insert(acc);
                 }
+                sum = max(sum, wmax);
             }
-        }
-        return sum;
-    }
-    int findrowmax(vector<int> w, int k) {
-        int sum = w[0], pre = 0, cur;
-        int erg = 0;  //遍历，因为k不一定是最大值
-        for(int i = 0; i < w.size(); i++) {
-            cur = max(w[i], w[i] + pre);
-            erg += w[i];
-            if(cur == k || erg == k) return k;
-            if(cur > k) return sum;
-            if(cur > sum) sum = cur;
-            swap(pre, cur);
         }
         return sum;
     }
