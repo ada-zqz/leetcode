0eecabe6fbca005336ae482f8fcf158548ede92d
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-20 10:54:35 +0800

    代码简化；判断是否有环的同时，找到重新开始的head和head+nL的位置，找到环的起点
    Runtime: 12 ms, faster than 77.18% of C++ online submissions for Linked List Cycle II.
    Memory Usage: 9.8 MB, less than 66.67% of C++ online submissions for Linked List Cycle II.

--- lk142_LinkedListCycleII.cpp
+++ lk142_LinkedListCycleII.cpp
@@ -10,31 +10,20 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         ListNode *slow = head, *fast = head;
-        bool cyc = false;
         while(fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
             if(slow == fast) {
-                cyc = true; 
-                break;
+                // s2 - s1 = nL，s2/s1是快/慢走的距离，L是环的长度，n是倍数
+                // s2 = 2s1 -> s1 = nL，s1的位置已经是环的整数倍
+                // 一个从头节点开始，一个从head+nL开始，遇到环的起点是会重合
+                slow = head;
+                while(slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-        }
-        if(cyc) {
-            int ncyc = 1;
-            slow = slow->next;
-            fast = fast->next->next;
-            while(slow != fast) {
-                ncyc += 1;
-                slow = slow->next;
-                fast = fast->next->next;
-            }
-            slow = head;
-            for(int i = 0; i < ncyc; i++) fast = fast->next;
-            while(slow != fast) {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return slow;
         }
         return NULL;
     }
