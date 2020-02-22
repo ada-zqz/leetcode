ee6641e580ed5a8588b4eefce14762ee6d800d13
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-22 15:50:58 +0800

    递归的写法
    Runtime: 12 ms, faster than 45.13% of C++ online submissions for Merge Two Sorted Lists.
    Memory Usage: 9.6 MB, less than 5.74% of C++ online submissions for Merge Two Sorted Lists.

--- lk21_MergeTwoSortedLists.cpp
+++ lk21_MergeTwoSortedLists.cpp
@@ -9,22 +9,15 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode lnew(0);
-        lnew.next = NULL;
-        
-        ListNode* ltmp = &lnew;
-        while(l1 && l2) {
-            if(l1->val < l2->val) {
-                ltmp->next = l1;
-                l1 = l1->next;
-            }
-            else {
-                ltmp->next = l2;
-                l2 = l2->next;
-            }
-            ltmp = ltmp->next;
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
         }
-        ltmp->next = l1 ? l1 : l2;
-        return lnew.next;
+        else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
     }
 };
