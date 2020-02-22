b245f81cd044463cb54ab6d38ae4680378a6dbd0
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-02-22 18:52:01 +0800

    不用long long，空间节省，但是时间增加；得到res前判断有没有越界
    Runtime: 4 ms, faster than 69.40% of C++ online submissions for Reverse Integer.
    Memory Usage: 8.2 MB, less than 97.14% of C++ online submissions for Reverse Integer.

--- math7_ReverseInteger.cpp
+++ math7_ReverseInteger.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long res = 0;
+        // int range [-2^31, 2^31 - 1]
+        if(x == INT_MIN) return 0;
+        int sign = x < 0 ? -1 : 1;
+        x = abs(x);
+        int res = 0;
         while(x) {
+            if(INT_MAX / 10 < res || (INT_MAX - x % 10) < res * 10)
+                return 0;
             res = res * 10 + x % 10;
             x = x / 10;
         } 
-        return (res < INT_MIN || res > INT_MAX) ? 0 : res;
+        return res*sign;
     }
 };
