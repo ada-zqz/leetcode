78243c278d4ea58b7790dd434aab0ee647944256
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-01 12:58:46 +0800

    递归，包括根节点和不包括根节点的情况
    Runtime: 20 ms, faster than 76.37% of C++ online submissions for Path Sum III.
    Memory Usage: 13.7 MB, less than 100.00% of C++ online submissions for Path Sum III.
    改善很明显啊

--- dfs437_PathSumIII.cpp
+++ dfs437_PathSumIII.cpp
@@ -11,20 +11,10 @@ class Solution {
 public:
     int pathSum(TreeNode* root, int sum) {
         if(!root) return 0;
-        vector<int> prev;
-        int res = 0;
-        dfs(root, prev, sum, res);
-        return res;
+        return pathsub(root, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
     }
-    void dfs(TreeNode* root, vector<int> prev, int sum, int& res) {
-        if(!root) return;
-        for(int i = 0; i < prev.size(); i++) {
-            prev[i] += root->val;
-            if(prev[i] == sum) res++;
-        }
-        prev.push_back(root->val);
-        if(root->val == sum) res++;
-        dfs(root->left, prev, sum, res);
-        dfs(root->right, prev, sum, res);
+    int pathsub(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return int(root->val == sum) + pathsub(root->left, sum - root->val) + pathsub(root->right, sum - root->val); //包括root->val
     }
 };
