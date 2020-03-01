4dffb4a01aada47b29839f6205da4c00312e524b
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-01 17:11:58 +0800

    递归，是数字while求出重复次数，不是数字一直add直到遇到“]”
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Decode String.
    Memory Usage: 8.2 MB, less than 100.00% of C++ online submissions for Decode String.

--- dp394_DecodeString.cpp
+++ dp394_DecodeString.cpp
@@ -1,61 +1,30 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> ks;
-        stack<string> cs;
-        int repeat = 0;
-        string rs = "", res = "";
-        int left = 0, right = 0;  //左右括号
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] >= '0' && s[i] <= '9' ) {
-                if(rs != "") {
-                    cs.push(rs);
-                    rs = "";
+        int i =0;
+        return decodesub(s, i);
+    }
+    string decodesub(const string& s, int& i) {
+        string res;
+        while(i < s.size() && s[i] != ']') {
+            if(isdigit(s[i])) {
+                int rp = 0;
+                while(i < s.size() && isdigit(s[i])) {
+                    rp = rp * 10 + s[i++] - '0';  //重复次数
                 }
-                repeat = repeat * 10 + s[i] - '0';
-            }
-            else if (s[i] >= 'a' && s[i] <= 'z') {
-                rs.append(&s[i], 1);
-            }
-            else if(s[i] == '[') {
-                left += 1;
-                ks.push(repeat);
-                repeat = 0;
-            }
-            else if(s[i] == ']') {
-                right += 1;
-                if(right < left) {
-                    int k = ks.top();
-                    ks.pop();
-                    string cnew = "";
-                    for(int j = 0; j < k; j++) {
-                        cnew.append(rs);
-                    }
-                    rs = "";
-                    string c = cs.top();
-                    cs.pop();
-                    c.append(cnew);
-                    cs.push(c);
+                i++; //上一行数字后对应'[', 再i++
+                string sub = decodesub(s, i);
+                i++; //过掉']'
+                
+                while(rp-- > 0) {
+                    res += sub;
                 }
-                else {
-                    if(rs != "") {
-                        cs.push(rs);
-                        rs = "";
-                    }
-                    int k = ks.top();
-                    ks.pop();
-                    string c = cs.top();
-                    cs.pop();
-                    cout<<k<<c<<endl;
-                    string cnew = "";
-                    for(int j = 0; j < k; j++) {
-                        cnew.append(c);
-                    }
-                    res.append(cnew);
-                }
+            }
+            else {
+                //字符
+                res += s[i++];
             }
         }
-        res.append(rs);  //最后出现字符的情况
         return res;
     }
 };
