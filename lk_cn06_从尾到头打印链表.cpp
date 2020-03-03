97452f2480ad7dcbb7da099c9a6814443d58f553
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-03 22:40:23 +0800

    res.insert(res.begin(), val)
    执行用时 : 72 ms, 在所有 C++ 提交中击败了12.24%的用户
    内存消耗 : 11.1 MB, 在所有 C++ 提交中击败了100.00%的用户

---
+++ lk_cn06_从尾到头打印链表.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+        vector<int> res;
+        while(head != NULL) {
+            res.insert(res.begin(), head->val);
+            head = head->next;
+        }
+        return res;
+    }
+};
