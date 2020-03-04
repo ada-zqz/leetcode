32d4e53cc76030cf63d458b01ed1b5b08f220ec7
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-04 08:48:07 +0800

    递归，时间很长
    Runtime: 504 ms, faster than 35.47% of C++ online submissions for Target Sum.
    Memory Usage: 8.1 MB, less than 100.00% of C++ online submissions for Target Sum.

--- dp494_TargetSum.cpp
+++ dp494_TargetSum.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+        for(auto n: nums) sum += n;
+        if(sum < S) return 0;
         int len = nums.size();
-        return dfs(len, nums, S);
+        return dfs(len - 1, nums, S);
     }
     int dfs(int i, vector<int>& nums, int s) {
         if(i == 0) {
