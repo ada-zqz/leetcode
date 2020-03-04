851b0fcab9a3ab18bec13625a94ce047c83ee982
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-04 08:49:29 +0800

    第二种方法，动态规划，dp[i]表示求和为i的组合数
    时间大幅缩短
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Target Sum.
    Memory Usage: 8 MB, less than 100.00% of C++ online submissions for Target Sum.

--- dp494_TargetSum.cpp
+++ dp494_TargetSum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
+        //https://leetcode.com/problems/target-sum/discuss/97334/Java-(15-ms)-C%2B%2B-(3-ms)-O(ns)-iterative-DP-solution-using-subset-sum-with-explanation
         int sum = 0;
         for(auto n: nums) sum += n;
-        if(sum < S) return 0;
-        int len = nums.size();
-        return dfs(len - 1, nums, S);
+        if(sum < S || sum < -S || (sum + S) % 2) return 0;
+        return findsum(nums, (sum + S) / 2);
     }
-    int dfs(int i, vector<int>& nums, int s) {
-        if(i == 0) {
-            int m = 0;
-            if(nums[i] == s) m += 1;
-            if(nums[i] == -s) m += 1;
-            return m;
+    int findsum(vector<int>& nums, int target) {
+        vector<int> dp(target + 1); //dp[i]表示求和为i的组合数
+        dp[0] = 1;
+        for(auto n: nums) {
+            for(int i = target; i >= n; i--) {
+                dp[i] += dp[i - n];
+            }
         }
-        return dfs(i - 1, nums, s - nums[i]) + dfs(i - 1, nums, s + nums[i]);
+        return dp[target];
     }
 };
