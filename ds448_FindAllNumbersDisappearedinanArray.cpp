80a94ab9151f05399f863efa432a1326141e4c93
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-05 09:18:38 +0800

    不新开数组表示数字有无出现，而是改变已有数组的正负来判断有无
    Runtime: 116 ms, faster than 71.58% of C++ online submissions for Find All Numbers Disappeared in an Array.
    Memory Usage: 14.2 MB, less than 100.00% of C++ online submissions for Find All Numbers Disappeared

--- ds448_FindAllNumbersDisappearedinanArray.cpp
+++ ds448_FindAllNumbersDisappearedinanArray.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<bool> exist(nums.size(), false);
-        for(auto n: nums) exist[n - 1] = true;
+        for(int i = 0; i < nums.size(); i++) {
+            int m = abs(nums[i]) - 1; //nums[i]对应数字m+1
+            nums[m] = -abs(nums[m]);   //m+1出现了，所以m+1的位置标为负数
+        }
         vector<int> now;
         for(int i = 0; i < nums.size(); i++) {
-            if(!exist[i]) now.push_back(i + 1);
+            if(nums[i] > 0) now.push_back(i + 1);
         }
         return now;
     }
