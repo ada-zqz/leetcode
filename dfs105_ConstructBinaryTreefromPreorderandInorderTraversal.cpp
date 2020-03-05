628b872d70a7363e217adc233f2fc14872d56883
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-05 22:42:02 +0800

    传index，中序遍历（压平）
    Runtime: 12 ms, faster than 97.09% of C++ online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
    Memory Usage: 21.7 MB, less than 23.81% of C++ online submissions for Construct Binary Tree from Preorder and Inorder Traversal.

--- dfs105_ConstructBinaryTreefromPreorderandInorderTraversal.cpp
+++ dfs105_ConstructBinaryTreefromPreorderandInorderTraversal.cpp
@@ -10,31 +10,24 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return buildsub(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+    }
+    TreeNode* buildsub(vector<int>& preorder, vector<int>& inorder, int pl, int pr, int il, int ir) {
         //前序遍历第一个是根节点，然后遍历左子树，右子树
         //中序遍历根节点左边是左子树，右边是右子树
-        if(preorder.empty()) return NULL;
-        TreeNode* root = new TreeNode(preorder[0]);
-        if(preorder.size() == 1) return root;
-        vector<int> inorder_l, inorder_r; //分别存放左右子树的节点
-        vector<int> preorder_l, preorder_r; //分别存放左右子树的节点
-        bool rcd_l = true;
-        for(int i = 0; i < inorder.size(); i++) {
+        if(pr < pl || pr < 0) return NULL;
+        TreeNode* root = new TreeNode(preorder[pl]);
+        if(pr == pl) return root;
+        int len;
+        for(int i = il; i <= ir; i++) {
             //找到root在中序遍历的位置
             if(inorder[i] == root->val) {
-                rcd_l = false;
-                continue;
-            }
-            if(rcd_l) {
-                inorder_l.push_back(inorder[i]);
-                preorder_l.push_back(preorder[i + 1]);
-            }
-            else {
-                inorder_r.push_back(inorder[i]);
-                preorder_r.push_back(preorder[i]);
+                len = i - il;
+                break;
             }
         }
-        root->left = buildTree(preorder_l, inorder_l);
-        root->right = buildTree(preorder_r, inorder_r);
+        root->left = buildsub(preorder, inorder, pl + 1, pl + len, il, il + len - 1);
+        root->right = buildsub(preorder, inorder, pl + len + 1, pr, il + len + 1, ir);
         return root;
     }
 };
