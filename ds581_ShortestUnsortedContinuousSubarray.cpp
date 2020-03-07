f1e9d41107d5f693682dedf21557808ae0db36f1
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-07 10:02:39 +0800

    O(n)的时间；从左到右，lmax[i]存储到i为止的最大值
    Runtime: 32 ms, faster than 93.21% of C++ online submissions for Shortest Unsorted Continuous Subarray.
    Memory Usage: 11.4 MB, less than 33.33% of C++ online submissions for Shortest Unsorted Continuous Subarray.

--- ds581_ShortestUnsortedContinuousSubarray.cpp
+++ ds581_ShortestUnsortedContinuousSubarray.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        //先排序，在前后比较
-        vector<int> sorted(nums);
-        sort(sorted.begin(), sorted.end());
         int len = nums.size();
+        vector<int> lmax(len), rmin(len);
+        lmax[0] = nums[0];
+        rmin[len - 1] = nums[len - 1];
+        //从左到右，lmax[i]存储到i为止的最大值
+        //从右到左，rmin[i]存储到i为止的最小值
+        for(int i = 1; i < len; i++) {
+            if(nums[i] > lmax[i - 1]) lmax[i] = nums[i];
+            else lmax[i] = lmax[i - 1];
+            if(nums[len - i - 1] < rmin[len - i]) rmin[len - i - 1] = nums[len - i - 1];
+            else rmin[len - i - 1] = rmin[len - i];
+        }
         int i = 0, j = len - 1;
-        while(i < len && nums[i] == sorted[i]) i++;
-        while(j > i && nums[j] == sorted[j]) j--;
+        while(i < len && nums[i] <= rmin[i]) i++;
+        while(j > i && nums[j] >= lmax[j]) j--;
         return j - i + 1;
     }
 };
