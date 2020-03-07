448a141028a1af71afc25ca07da204dfc2ac34e7
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-07 10:20:40 +0800

    O(n)时间，O(1)空间
    Runtime: 32 ms, faster than 93.21% of C++ online submissions for Shortest Unsorted Continuous Subarray.
    Memory Usage: 9.7 MB, less than 100.00% of C++ online submissions for Shortest Unsorted Continuous Subarray.

--- ds581_ShortestUnsortedContinuousSubarray.cpp
+++ ds581_ShortestUnsortedContinuousSubarray.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int len = nums.size();
-        vector<int> lmax(len), rmin(len);
-        lmax[0] = nums[0];
-        rmin[len - 1] = nums[len - 1];
-        //从左到右，lmax[i]存储到i为止的最大值
-        //从右到左，rmin[i]存储到i为止的最小值
+        int nmax = nums[0], nmin = nums[len - 1];
+        int s = -1, e = -2;  //e - s + 1 = 0
+        //从左到右，nums[i]比最大值要小的要排序
+        //从右到左，nums[i]比最小值要大的要排序
         for(int i = 1; i < len; i++) {
-            if(nums[i] > lmax[i - 1]) lmax[i] = nums[i];
-            else lmax[i] = lmax[i - 1];
-            if(nums[len - i - 1] < rmin[len - i]) rmin[len - i - 1] = nums[len - i - 1];
-            else rmin[len - i - 1] = rmin[len - i];
+            nmax = max(nmax, nums[i]);
+            nmin = min(nmin, nums[len - i - 1]);
+            if(nums[i] < nmax) e = i;  //一直找到最右边要排序的值
+            if(nums[len - i - 1] > nmin) s = len - i - 1;  //一直找到最左边要排序的值
         }
-        int i = 0, j = len - 1;
-        while(i < len && nums[i] <= rmin[i]) i++;
-        while(j > i && nums[j] >= lmax[j]) j--;
-        return j - i + 1;
+        return e - s + 1;
     }
 };
