d39c6b554a27f4a762331d859ce9d1f45d9a88db
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-16 09:59:38 +0800

    用到动态规划的思想，代码更简单，更快
    Runtime: 4 ms, faster than 98.07% of C++ online submissions for Best Time to Buy and Sell Stock.
    Memory Usage: 8.8 MB, less than 100.00% of C++ online submissions for Best Time to Buy and Sell Stock.

--- dp121_BestTimetoBuyandSellStock.cpp
+++ dp121_BestTimetoBuyandSellStock.cpp
@@ -2,31 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         if(prices.size() < 2) {return 0;}
-        int buy = prices[0], sell = prices[0];
-        int pro = sell - buy;
-        int temp = -1;
+        int sell = -prices[0];
+        int buy = prices[0];
         for(int i = 1; i < prices.size(); i++) {
-            if(prices[i] - sell > 0) {
-                sell = prices[i];
-                pro = sell - buy;
-            }
-            if((temp != -1) && ((prices[i] - temp) > pro)) {
-                buy = temp;
-                sell = prices[i];
-                pro = sell - buy;
-                temp = -1;
-            }
-            if(prices[i] - buy < 0) {
-                if(temp == -1) {
-                    temp = prices[i];
-                }
-                else{
-                    if(prices[i] < temp) {
-                        temp = prices[i];
-                    }
-                }
-            }
+            sell = max(sell, prices[i] - buy);
+            buy = min(buy, prices[i]);
         }
-        return pro;
+        return max(0, sell);
     }
 };
