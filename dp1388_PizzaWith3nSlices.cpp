843af18c5f942b1511f8762afb8d36aa14606d3e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-03-24 18:08:05 +0800

    dp[i][j] 到i时选择j个蛋糕的最大值
    Runtime: 24 ms, faster than 67.61% of C++ online submissions for Pizza With 3n Slices.
    Memory Usage: 12.6 MB, less than 100.00% of C++ online submissions for Pizza With 3n Slices.

--- dp1388_PizzaWith3nSlices.cpp
+++ dp1388_PizzaWith3nSlices.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     int ans = 0;
     int maxSizeSlices(vector<int>& slices) {
-        sub(slices, 0);
-        return ans;
+        int n = slices.size() / 3;
+        // 选中的蛋糕不能相邻
+        // 首尾不能同时选中
+        vector<int> l1 = vector<int>(slices.begin(), slices.end() - 1);
+        vector<int> l2 = vector<int>(slices.begin() + 1, slices.end());
+        return max(sub(l1, n), sub(l2, n));
     }
-    void sub(vector<int> slices, int pre) {
-        int n = slices.size();
-        if(n == 0) {
-            if(pre > ans) ans = pre;
-            return;
-        }
-        if(n == 3) {
-            int m = max(slices[0], slices[1]);
-            m = max(m, slices[2]);
-            if(pre + m > ans) ans = pre + m;
-            return;
-        }
-        vector<int> tmp(n - 3);
-        for(int i = 0; i < n; i++) {
-            for(int k = 0; k < n - 3; k++) {
-                tmp[k] = slices[(i + 2 + k) % n];
+    int sub(vector<int> v, int n) {
+        // dp[i][j]: 截至[1,i], 选取j个蛋糕得到的最大值
+        int m = v.size() + 2;
+        int ans = 0;
+        vector<vector<int>> dp(m, vector<int>(n + 1, 0));
+        for(int i = 2; i < m; i++) {
+            for(int j = 1; j <= n; j++) {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 2][j - 1] + v[i - 2]);
             }
-            sub(tmp, slices[i] + pre);
+            ans = max(ans, dp[i][n]);
         }
+        return ans;
     }
 };
