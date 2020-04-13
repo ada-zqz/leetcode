248106e8b55b28794d4c1efa75ea2058802d7221
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-13 12:21:30 +0800

    keep a max-product-window less than k
    Runtime: 132 ms, faster than 56.76% of C++ online submissions for Subarray Product Less Than K.
    Memory Usage: 16 MB, less than 100.00% of C++ online submissions for Subarray Product Less Than K.

--- tp713_SubarrayProductLessThanK.cpp
+++ tp713_SubarrayProductLessThanK.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k == 0) return 0;
         int res = 0;
         int n = nums.size();
-        int r = 0;
         int mul = 1;
-        for(int i = 0; i < n; i++) {
-            if(i >= r) {
-                mul = 1;  //重新开始乘
-                r = i;
+        // 前一种解法是，nums[i], nums[i, i + 1], ..., nums[i, ..., j]
+        // 乘到j时 < k, res += j - i + 1  nums[j], nums[j, j-1], ..., nums[j, ..., i]满足条件
+        // 乘到j时 >= k, i++
+        for(int i = 0, j = 0; j < n; j++) {
+            mul *= nums[j];
+            while(i <= j && mul >= k) {
+                mul /= nums[i++];
             }
-            else mul /= nums[i - 1];
-            res += (r - i);  //就j < k 之前算过肯定小于k
-            for(int j = r; j < n; j++) {
-                mul *= nums[j];
-                if(mul < k) {
-                    res++;
-                    if(j == n - 1) r = n;
-                }
-                else {
-                    r = j;
-                    mul /= nums[j];
-                    break;
-                }
-            }
+            res += j - i + 1;  //i=j+1时为0
         }
         return res;
     }
