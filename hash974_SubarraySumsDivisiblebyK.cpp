1acf04adcb21d1cbde1aa53d8e50a9ece8346099
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-13 20:59:45 +0800

    sum[0, j] mod=2，cnt[2] = c说明有sum[0, i] mod = 2存在c种
    Runtime: 56 ms, faster than 85.27% of C++ online submissions for Subarray Sums Divisible by K.
    Memory Usage: 10.2 MB, less than 100.00% of C++ online submissions for Subarray Sums Divisible by K.

--- hash974_SubarraySumsDivisiblebyK.cpp
+++ hash974_SubarraySumsDivisiblebyK.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& A, int K) {
-        int n = A.size();
+        // 之前固定i，考虑j增加对应的res
+        // 另一个思路，固定j，考虑之前计算的i的结果
         int res = 0;
-        unordered_map<int, int> mp;
-        mp.insert({0, 0});
-        vector<int> sm(n + 1, 0);
-        for(int i = 0; i < n; i++) {
-            sm[i + 1] = sm[i] + A[i];
-            int mod = (sm[i + 1] % K + K) % K; //开始mod会有负数
-            if(mp.find(mod) == mp.end()) mp.insert({mod, 1});
-            else mp[mod]++;
-        }
-        res += mp[0];
-        for(int i = 1; i < n; i++) {
-            int modnew = (sm[i] % K + K) % K;
-            mp[modnew]--;
-            res += mp[modnew];
+        int n = A.size();
+        vector<int> count(K, 0);
+        count[0] = 1; // id表示mod出现的次数
+        int pre = 0;
+        for(int a: A) {
+            pre = ((pre + a) % K + K) % K;  // 保证非负
+            res += count[pre]++;  // 比如现在sum[0, j]mod后为2，count[2] = c说明有sum[0, i] mod = 2存在c种
         }
         return res;
     }
