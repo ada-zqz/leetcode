9b7dc0ff048f6d85469fdcda0ada918a645c63d1
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-17 11:41:36 +0800

    visited没有引用，超时
    Runtime: 108 ms, faster than 47.06% of C++ online submissions for Word Search.
    Memory Usage: 20.8 MB, less than 50.00% of C++ online submissions for Word Search.

--- dfs79_WordSearch.cpp
+++ dfs79_WordSearch.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
         int m = board.size(), n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 // 从[i, j]开始成不成功
@@ -11,21 +11,19 @@ public:
         }
         return false;
     }
-    bool sub(vector<vector<char>>& board, vector<vector<bool>> visited, string word, int i, int j) {
+    bool sub(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int i, int j) {
         if(word.size() == 0) return true;
         int m = board.size(), n = board[0].size();
-        if(i < 0 || j < 0 || i >= m || j >= n) return false;
-        if(word[0] != board[i][j]) return false;
+        // 超出边界，访问过了，不等于word[0]
+        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || word[0] != board[i][j]) return false;
         
-        if(!visited[i][j]) {
-            string ws = word.substr(1, word.size() - 1);
-            visited[i][j] = true;
-            if(sub(board, visited, ws, i + 1, j)) return true;
-            if(sub(board, visited, ws, i - 1, j)) return true;
-            if(sub(board, visited, ws, i, j + 1)) return true;
-            if(sub(board, visited, ws, i, j - 1)) return true;
-            visited[i][j] = false;
-        }
+        string ws = word.substr(1, word.size() - 1);
+        visited[i][j] = true;
+        if(sub(board, visited, ws, i + 1, j)) return true;
+        if(sub(board, visited, ws, i - 1, j)) return true;
+        if(sub(board, visited, ws, i, j + 1)) return true;
+        if(sub(board, visited, ws, i, j - 1)) return true;
+        visited[i][j] = false;
         return false;
     }
 };
