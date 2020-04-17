ac42e71cc0ff26dc921e13f05fc75faf9f8bdd2f
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-17 11:45:47 +0800

    不新开visited，而是在原有board数组上赋值表示是否访问过，快20ms
    Runtime: 84 ms, faster than 51.26% of C++ online submissions for Word Search.
    Memory Usage: 21 MB, less than 50.00% of C++ online submissions for Word Search.

--- dfs79_WordSearch.cpp
+++ dfs79_WordSearch.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size(), n = board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 // 从[i, j]开始成不成功
-                if(sub(board, visited, word, i, j)) return true;
+                if(sub(board, word, i, j)) return true;
             }
         }
         return false;
     }
-    bool sub(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int i, int j) {
+    bool sub(vector<vector<char>>& board, string word, int i, int j) {
         if(word.size() == 0) return true;
         int m = board.size(), n = board[0].size();
         // 超出边界，访问过了，不等于word[0]
-        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || word[0] != board[i][j]) return false;
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '*' || word[0] != board[i][j]) return false;
         
         string ws = word.substr(1, word.size() - 1);
-        visited[i][j] = true;
-        if(sub(board, visited, ws, i + 1, j)) return true;
-        if(sub(board, visited, ws, i - 1, j)) return true;
-        if(sub(board, visited, ws, i, j + 1)) return true;
-        if(sub(board, visited, ws, i, j - 1)) return true;
-        visited[i][j] = false;
+        char tmp = board[i][j];
+        board[i][j] = '*';
+        if(sub(board, ws, i + 1, j)) return true;
+        if(sub(board, ws, i - 1, j)) return true;
+        if(sub(board, ws, i, j + 1)) return true;
+        if(sub(board, ws, i, j - 1)) return true;
+        board[i][j] = tmp;
         return false;
     }
 };
