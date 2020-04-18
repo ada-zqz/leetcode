6b4da9b75a70d275189427f4c99be903ce545a09
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-18 21:09:13 +0800

    Create LCP06_拿硬币.cpp

---
+++ LCP06_拿硬币.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCount(vector<int>& coins) {
+        int res = 0;
+        for(int i = 0; i < coins.size(); i++) {
+            if(coins[i] % 2 == 0) 
+                res += coins[i] / 2;
+            else
+                res += (coins[i] + 1) / 2;
+        }
+        return res;
+    }
+};
