b3d734ce0c633750c37b60bb51dc5eae9cfcb600
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-21 12:19:11 +0800

    优化后的BFS还是超时

--- LCP09_最小跳跃次数.cpp
+++ LCP09_最小跳跃次数.cpp
@@ -3,19 +3,25 @@ public:
     int minJump(vector<int>& jump) {
         // BFS
         int n = jump.size();
-        set<int> st;
+        set<pair<int, int>> st; //now, pre
         vector<bool> visited(n, false);
-        st.insert(0);
+        st.insert({0, 0});
         int step = 0;
         while(!st.empty()) {
             step++;
-            set<int> newst;            
-            for(int id: st) {
+            set<pair<int, int>> newst;            
+            for(auto state: st) {
+                int id = state.first;
+                int pre = state.second;
+                // if(visited[id]) continue;                // 之前访问过
                 visited[id] = true;
-                if(id + jump[id] >= n) return step;
-                newst.insert(id + jump[id]);
-                for(int i = 0; i < id; i++) {
-                    if(!visited[i]) newst.insert(i);
+                int right = id + jump[id];                  // 向右跳
+                if(right >= n) return step;
+                // 之前没有访问过right
+                if(!visited[right]) newst.insert({right, id});   
+                if(pre >= id) continue;                     // 之前更新过[0, pre]的值
+                for(int i = pre + 1; i < id; i++) {         // 向左跳
+                    if(!visited[i]) newst.insert({i, id});
                 }
             }
             swap(st, newst);
