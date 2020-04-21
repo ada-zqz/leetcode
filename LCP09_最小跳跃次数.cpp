261a7ba9878dacf73bed5dd94cfb24b56eb85fa9
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-21 13:05:31 +0800

    BFS再次优化，pre不是当前index的前一步，而是[0, pre]已经更新过的最大pre值
    执行用时 :
    848 ms, 在所有 C++ 提交中击败了35.26%的用户
    内存消耗 :
    136 MB, 在所有 C++ 提交中击败了100.00%的用户

--- LCP09_最小跳跃次数.cpp
+++ LCP09_最小跳跃次数.cpp
@@ -3,26 +3,28 @@ public:
     int minJump(vector<int>& jump) {
         // BFS
         int n = jump.size();
-        set<pair<int, int>> st; //now, pre
+        queue<int> st; // now index
         vector<bool> visited(n, false);
-        st.insert({0, 0});
+        st.push(0);
+        int pre = -1;      // [0, pre]已经更新过
         int step = 0;
         while(!st.empty()) {
             step++;
-            set<pair<int, int>> newst;            
-            for(auto state: st) {
-                int id = state.first;
-                int pre = state.second;
+            queue<int> newst;            
+            while(!st.empty()) {
+                int id = st.front();
+                st.pop();
                 // if(visited[id]) continue;                // 之前访问过
                 visited[id] = true;
                 int right = id + jump[id];                  // 向右跳
                 if(right >= n) return step;
                 // 之前没有访问过right
-                if(!visited[right]) newst.insert({right, id});   
+                if(!visited[right]) newst.push(right);   
                 if(pre >= id) continue;                     // 之前更新过[0, pre]的值
                 for(int i = pre + 1; i < id; i++) {         // 向左跳
-                    if(!visited[i]) newst.insert({i, id});
+                    if(!visited[i]) newst.push(i);
                 }
+                pre = id;
             }
             swap(st, newst);
         }
