45706975c1d5d50d956e44ddddea9f2d9d069554
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-04-21 13:24:36 +0800

    动态规划比BFS快很多
    执行用时 :
    492 ms, 在所有 C++ 提交中击败了93.38%的用户
    内存消耗 :
    110.4 MB, 在所有 C++ 提交中击败了100.00%的用户

--- LCP09_最小跳跃次数.cpp
+++ LCP09_最小跳跃次数.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int minJump(vector<int>& jump) {
-        // BFS
         int n = jump.size();
-        queue<int> st; // now index
-        vector<bool> visited(n, false);
-        st.push(0);
-        int pre = -1;      // [0, pre]已经更新过
-        int step = 0;
-        while(!st.empty()) {
-            step++;
-            queue<int> newst;            
-            while(!st.empty()) {
-                int id = st.front();
-                st.pop();
-                // if(visited[id]) continue;                // 之前访问过
-                visited[id] = true;
-                int right = id + jump[id];                  // 向右跳
-                if(right >= n) return step;
-                // 之前没有访问过right
-                if(!visited[right]) newst.push(right);   
-                if(pre >= id) continue;                     // 之前更新过[0, pre]的值
-                for(int i = pre + 1; i < id; i++) {         // 向左跳
-                    if(!visited[i]) newst.push(i);
-                }
-                pre = id;
-            }
-            swap(st, newst);
+        vector<int> dp(n, n);
+        dp[n - 1] = 1;
+        for(int i = n - 2; i >= 0; i--) {
+            if(i + jump[i] >= n) dp[i] = 1;
+            else dp[i] = 1 + dp[i + jump[i]];
+            // 借助[i]更新[i+1, n-1]能到达n的最小值
+            for(int j = i + 1; j < n && dp[j] >= 1 + dp[i]; j++)    
+                dp[j] = 1 + dp[i];
         }
-        return -1;
+        return dp[0];
     }
 };
