5351cbf91e502277f1d18d29046c10062ef59975
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-01 12:48:03 +0800

    dp[i][j]
    Runtime: 16 ms, faster than 37.75% of C++ online submissions for Distinct Subsequences.
    Memory Usage: 13.4 MB, less than 75.00% of C++ online submissions for Distinct Subsequences.

---
+++ dp115_DistinctSubsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;  // 相当于t为空时都能匹配上
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i && j <= m; j++) {
+                if(s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
