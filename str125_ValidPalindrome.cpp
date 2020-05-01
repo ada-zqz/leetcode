9570dac9ce95874440f0b123c9dc44201949728e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-01 18:14:52 +0800

    用已有的函数，比自己写的代码简洁；isalnum, toupper
    Runtime: 12 ms, faster than 29.97% of C++ online submissions for Valid Palindrome.
    Memory Usage: 7.2 MB, less than 100.00% of C++ online submissions for Valid Palindrome.

--- str125_ValidPalindrome.cpp
+++ str125_ValidPalindrome.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = s.size();
-        for(int i = 0, j = n - 1; i < n && j >=0; ) {
-            while(!isalpha(s[i]) && i < n ) {
-                i++;
-                if(i == n) break;
-            }
-            while(!isalpha(s[j]) && j >= 0) {
-                j--;
-                if(j < 0) break;
-            }
-            if(i >= j) return true;            
-            if(isequal(s[i], s[j])) {
-                i++;
-                j--;
-            }
-            else return false;
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) { 
+            while(!isalnum(s[i]) && i < j) i++; 
+            while(!isalnum(s[j]) && i < j) j--; 
+            if(toupper(s[i]) != toupper(s[j])) return false; 
         }
-        return true;  //""
-    }
-    bool isequal(char & a, char & b) {
-        if(isdigit(a) && isdigit(b)) {
-            if(a == b) return true;
-            else return false;
-        }
-        else if(!isdigit(a) && !isdigit(b)) {
-            if(a == b or abs(a - b) == 32) return true;
-            else return false;
-        }
-        
-        return false;
-    }
-    bool isalpha(char & a) {
-        if(isdigit(a)) return true;
-        if((a >= 'a' && a <= 'z') or (a >= 'A' && a <= 'Z')) return true;
-        return false;
-    }                  
+        return true;
+    }               
 };
