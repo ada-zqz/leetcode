cfc8be6bbc83b737425d5b5942933f9a2469992d
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-03 11:04:27 +0800

    超时

--- wc5402_LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.cpp
+++ wc5402_LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int res = 1;
+        vector<int> pre(n, -1);
+        vector<int> now(n, -1);
         for(int j = 0; j < n; j++) {
-            dp[j][j] = 0;
+            now[j] = 0;
             for(int i = j - 1; i >= 0; i--) {
                 int diff = abs(nums[i] - nums[j]);
-                dp[i][j] = max(dp[i + 1][j], max(dp[i][j - 1], diff));
+                now[i] = max(now[i + 1], max(pre[i], diff));
+                if(now[i] <= limit) res = max(res, j - i + 1);
             }
-        }
-        int res = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(dp[i][j] <= limit) res = max(res, j - i + 1);
-            }
+            pre = now;
         }
         return res;
     }
