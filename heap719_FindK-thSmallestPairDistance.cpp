7131bae880cb6eaa7f3de7c5555f1ab99723350b
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-05 14:53:30 +0800

    全部都算出来保持在count[diff]的数组中 O(n^2)
    Runtime: 1036 ms, faster than 8.17% of C++ online submissions for Find K-th Smallest Pair Distance.
    Memory Usage: 14.5 MB, less than 8.33% of C++ online submissions for Find K-th Smallest Pair Distance.

--- heap719_FindK-thSmallestPairDistance.cpp
+++ heap719_FindK-thSmallestPairDistance.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
-        set<pair<int, vector<int>>> mp; // diff, [i, j]
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = 0; i < n - 1; i++) 
-            mp.insert({nums[i + 1] - nums[i], vector<int>{i, i + 1}});
-        int id = 0;
-        auto p = *mp.begin();
-        while(id < k) {
-            p = *mp.begin();
-            mp.erase(mp.begin());
-            id++;
-            if(p.second[1] < n - 1) {
-                auto cp = p.second;
-                cp[1]++;
-                mp.insert({nums[cp[1]] - nums[cp[0]], cp});
+        int count[1000005];
+        memset(count, 0, sizeof(count));
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                count[abs(nums[j] - nums[i])]++;
             }
         }
-        return p.first;
+        for(int i = 0; i <= 1e6; i++) {
+            if(count[i] >= k) return i;
+            k -= count[i];
+        }
+        return -1;
     }
 };
