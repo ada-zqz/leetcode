4b054f37610838aaa23cb2c1fb339177a852fc25
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-06 16:43:40 +0800

    加了4min>target和4max<target的判断快了一些
    Runtime: 76 ms, faster than 38.21% of C++ online submissions for 4Sum.
    Memory Usage: 26.9 MB, less than 21.05% of C++ online submissions for 4Sum.

--- hash18_4Sum.cpp
+++ hash18_4Sum.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        unordered_map<int, set<pair<int, int>>> mp;  //sum, (i,j)
+        vector<vector<int>> res;
         int n = nums.size();
+        sort(nums.begin(), nums.end());
+        if(n == 0 || 4 * nums[0] > target || 4 * nums[n - 1] < target) return res;
         // 先算出两两相加的值
+        unordered_map<int, set<pair<int, int>>> mp;  //sum, (i,j)
         for(int i = 0; i < n - 1; i++) {
             for(int j = i + 1; j < n; j++) {
                 mp[nums[i] + nums[j]].insert({i, j});
@@ -17,11 +20,11 @@ public:
                 if(mp.find(target - cd) != mp.end()) {
                     auto st = mp[target - cd];
                     for(auto s: st) {
-                    // index不重复
+                        // index不重复
                         if(s.first > i && s.first != j && s.second != j) {
-                            v = vector<int>{nums[i], nums[j], nums[s.first], nums[s.second]};   
-                            // 值不重复
+                            v = vector<int>{nums[i], nums[j], nums[s.first], nums[s.second]};          
                             sort(v.begin(), v.end());
+                            // 最终结果不重复
                             ans.insert(v);
                         }
                                           
@@ -29,7 +32,6 @@ public:
                 }
             }
         }
-        vector<vector<int>> res;
         for(auto s: ans) res.push_back(s);
         return res;
     }
