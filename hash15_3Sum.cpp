7186f6667c1c7a228d13620fa3c383d67cfb3fed
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-06 19:43:34 +0800

    unordered_map<int, set<pair<int, int>>> 超时

--- hash15_3Sum.cpp
+++ hash15_3Sum.cpp
@@ -6,25 +6,25 @@ public:
         int n = nums.size();
         if(n < 3 || 3 * nums[0] > 0 || 3 * nums[n - 1] < 0) return res;
         
+        unordered_map<int, set<pair<int, int>>> mp;  //sum, (i,j)
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int sm = nums[i] + nums[j];
+                mp[sm].insert({i, j});
+            }
+        }
+        set<vector<int>> ans;
         for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue; //重复，前一个i考虑的范围更广，肯定已经包括了
-            int a = nums[i];
-            if(a + 2 * nums[i + 1] > 0) break; //最小的情况已经不满足sum<=0
-            for(int j = i + 1; j < n - 1; j++) {
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue; //重复
-                int b = a + nums[j];
-                if(b + nums[j + 1] > 0) break;
-                for(int k = j + 1; k < n; k++) {
-                    if(k > j + 1 && nums[k] == nums[k - 1]) continue;
-                    int c = b + nums[k];
-                    if(c > 0) break;
-                    else if(c == 0) {
-                        res.push_back(vector<int>{a, b - a, nums[k]});
-                        break;
-                    }
+            if(i > 0 && nums[i] == nums[i - 1]) continue;  //前一个i已经包含了
+            if(mp.find(-nums[i]) != mp.end()) {
+                auto st = mp[-nums[i]];
+                for(auto s: st) {
+                    if(s.first > i) 
+                        ans.insert(vector<int>{nums[i], nums[s.first], nums[s.second]});
                 }
             }
         }
+        for(auto v: ans) res.push_back(v);
         return res;
     }
 };
