5e6c3e558f788c2b46c387b4ecd77ae2e3579891
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-06 19:51:41 +0800

    修改判断重复的语句还是超时

--- hash15_3Sum.cpp
+++ hash15_3Sum.cpp
@@ -7,24 +7,35 @@ public:
         if(n < 3 || 3 * nums[0] > 0 || 3 * nums[n - 1] < 0) return res;
         
         unordered_map<int, set<pair<int, int>>> mp;  //sum, (i,j)
-        for(int i = 0; i < n - 1; i++) {
+        for(int i = 1; i < n - 1; i++) {
             for(int j = i + 1; j < n; j++) {
                 int sm = nums[i] + nums[j];
                 mp[sm].insert({i, j});
             }
         }
-        set<vector<int>> ans;
         for(int i = 0; i < n - 2; i++) {
             if(i > 0 && nums[i] == nums[i - 1]) continue;  //前一个i已经包含了
             if(mp.find(-nums[i]) != mp.end()) {
                 auto st = mp[-nums[i]];
                 for(auto s: st) {
-                    if(s.first > i) 
-                        ans.insert(vector<int>{nums[i], nums[s.first], nums[s.second]});
+                    // i, s.f, s.s
+                    if(s.first > i) {
+                        if(res.size() == 0) {
+                            res.push_back(vector<int>{nums[i], nums[s.first], nums[s.second]});
+                        }
+                        else {
+                            auto v = res.back();
+                            if(v[0] != nums[i]) {
+                                res.push_back(vector<int>{nums[i], nums[s.first], nums[s.second]});
+                            }
+                            else if(v[1] != nums[s.first]) {
+                                res.push_back(vector<int>{nums[i], nums[s.first], nums[s.second]});
+                            }
+                        }
+                    }
                 }
             }
         }
-        for(auto v: ans) res.push_back(v);
         return res;
     }
 };
