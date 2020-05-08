4846be410eccf469837b601eb5f225626524bcc0
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-08 15:08:26 +0800

    找到cnt>=k的mid的最小值，那这个值一定在table里
    Runtime: 8 ms, faster than 99.47% of C++ online submissions for Kth Smallest Number in Multiplication Table.
    Memory Usage: 6.1 MB, less than 100.00% of C++ online submissions for Kth Smallest Number in Multiplication Table.

---
+++ bs668_KthSmallestNumberinMultiplicationTable.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int lo = 1, hi = m * n;
+        // 找到cnt>=k的mid的最小值，那这个值一定在table里
+        while(lo < hi) {
+            int mid = (lo + hi) / 2;
+            int cnt = 0;
+            for(int i = 1, j = n; i <= m; i++) {
+                while(i * j > mid && j > 0) j--;
+                if(j == 0) break;
+                cnt += j;
+            }
+            if(cnt > k) hi = mid; //mid-1有误，因为等于mid的可能有好几个，导致[lo, mid-1]的cnt<k  
+            else if(cnt < k) lo = mid + 1;
+            else {
+                // cnt == k
+                if(lo == mid) return lo; //[lo(mid), hi]，mid就是最小的
+                else {
+                    //[lo, mid, hi], [lo, mid]区间内一定有cnt<=k
+                    hi = mid;
+                }
+            }
+        }
+        return lo; 
+    }
+};
