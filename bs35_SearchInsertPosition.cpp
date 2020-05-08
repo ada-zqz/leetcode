f7e2acac8714a11f2a6cbf3cb67e6f87a0739b89
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-08 13:23:23 +0800

    应该在的index，hi=mid-1(一般不-1，这里循环判断为lo<=hi)，返回lo
    Runtime: 8 ms, faster than 26.69% of C++ online submissions for Search Insert Position.
    Memory Usage: 9.6 MB, less than 9.38% of C++ online submissions for Search Insert Position.

--- bs35_SearchInsertPosition.cpp
+++ bs35_SearchInsertPosition.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int lo = 0, hi = nums.size() - 1;
-        while(lo < hi) {
+        while(lo <= hi) {
             int mid = (lo + hi) / 2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) hi = mid;
+            else if(nums[mid] > target) hi = mid - 1; 
+            //target < nums[hi + 1],位置应该在[lo, hi + 1]之间
+            //若在hi+1，lo=hi后,lo++等于应该在的位置
             else lo = mid + 1;
+            //target > nums[lo - 1],位置应该在[lo, hi]之间
         }
-        if(nums[lo] >= target) 
-            return lo;
-        return lo + 1;
+        return lo;
     }
 };
