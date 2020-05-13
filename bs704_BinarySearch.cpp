7990c165b647d7c86e1975e43f5c82689271e65d
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-13 11:15:57 +0800

    Create bs704_BinarySearch.cpp
    Runtime: 92 ms, faster than 11.62% of C++ online submissions for Binary Search.
    Memory Usage: 27.6 MB, less than 5.56% of C++ online submissions for Binary Search.

---
+++ bs704_BinarySearch.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        while(lo < hi) {
+            int mid = (lo + hi) / 2;
+            if(nums[mid] < target) lo = mid + 1;
+            else if(nums[mid] > target) hi = mid;
+            else return mid;
+        }
+        if(nums[lo] == target) return lo;
+        return -1;
+    }
+};
