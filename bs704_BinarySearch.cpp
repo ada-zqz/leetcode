74da95b0f2964178d5f942b3b14f4ea82fbae3d8
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-13 11:21:30 +0800

    while(lo <= hi)
    Runtime: 84 ms, faster than 22.65% of C++ online submissions for Binary Search.
    Memory Usage: 27.7 MB, less than 5.56% of C++ online submissions for Binary Search.

--- bs704_BinarySearch.cpp
+++ bs704_BinarySearch.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int lo = 0, hi = nums.size() - 1;
-        while(lo < hi) {
+        while(lo <= hi) {
             int mid = (lo + hi) / 2;
             if(nums[mid] < target) lo = mid + 1;
             else if(nums[mid] > target) hi = mid - 1;
             else return mid;
         }
-        if(nums[lo] == target) return lo;
         return -1;
     }
 };
