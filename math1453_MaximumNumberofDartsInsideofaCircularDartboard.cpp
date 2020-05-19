4a94cbaf72a6b19139c6b01f00c4be00d996af69
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-19 14:57:53 +0800

    第二种方法：扫描，O(n^2 lgn) lgn是排序的需要；atan2
    Runtime: 100 ms, faster than 55.03% of C++ online submissions for Maximum Number of Darts Inside of a Circular Dartboard.
    Memory Usage: 13.9 MB, less than 100.00% of C++ online submissions for Maximum Number of Darts Inside of a Circular Dartboard.

--- math1453_MaximumNumberofDartsInsideofaCircularDartboard.cpp
+++ math1453_MaximumNumberofDartsInsideofaCircularDartboard.cpp
@@ -1,65 +1,48 @@
 class Solution {
 public:
-    // 两点之间距离的平方
-    int dis2(vector<int>& v1, vector<int>& v2) {
-        return (v1[0] - v2[0]) * (v1[0] - v2[0]) + (v1[1] - v2[1]) * (v1[1] - v2[1]);
-    }
-    // 在圆里
-    bool incirc(vector<int>& v, vector<double>& ct, int& r2) {
-        if(pow(ct[0] - (double)v[0], 2) + pow(ct[1] - (double)v[1], 2) <= (double)r2) return true;
-        return false;
-    }
-    // 圆心位置
-    vector<vector<double>> center(vector<int>& v1, vector<int>& v2, int& r2, int& dis) {
-        //两个圆心
-        vector<vector<double>> ct(2, vector<double>(2));
-        //两个点中点到圆心之间的距离
-        double dc = pow((double)r2 - (double)dis / 4, 0.5);
-        //两点中点坐标
-        vector<double> mid(2);
-        mid[0] = (double)(v1[0] + v2[0]) / 2;
-        mid[1] = (double)(v1[1] + v2[1]) / 2;
-        double tantheta, costheta, sintheta;
-        if(v1[1] != v2[1]) {
-            tantheta = (double)(v2[0] - v1[0]) / (v1[1] - v2[1]);  //k1*k2=-1
-            if(tantheta >= 0) {
-                costheta = 1. / pow(tantheta * tantheta + 1., 0.5);
-                sintheta = tantheta * costheta;
-            }
-            else {
-                costheta = -1. / pow(tantheta * tantheta + 1., 0.5); //<0
-                sintheta = tantheta * costheta;                      //>0
-            }
+    // const double PI = atan(1.0)*4;  // M_PI
+    // 给pair<double, int>排序，记录扫描的最大点数
+    struct cmp {
+        bool operator()(pair<double, int>& a1, pair<double, int>& a2) {
+            if(a1.first != a2.first) return a1.first < a2.first;
+            return a1.second > a2.second;  //可以在圆线上，先进来，下一个才考虑出去
         }
-        else {
-            sintheta = 1;
-            costheta = 0;
-        }
-        ct[0][0] = mid[0] - dc * costheta;
-        ct[0][1] = mid[1] - dc * sintheta;
-        ct[1][0] = mid[0] + dc * costheta;
-        ct[1][1] = mid[1] + dc * sintheta;
-        return ct;
+    };
+    // 两点之间距离
+    double dist(vector<int>& v1, vector<int>& v2) {
+        return sqrt((double)pow(v1[0] - v2[0], 2) + pow(v1[1] - v2[1], 2));
+    }
+    // 进入圆的角1和角2
+    vector<double> findangle(vector<int>& a, vector<int>& b, double& delta) {
+        double angleB = atan2(b[1] - a[1], b[0] - a[0]);  // [-pi, pi]
+        double angle1 = angleB - delta;
+        double angle2 = angleB + delta;
+        return vector<double> {angle1, angle2};
     }
     int numPoints(vector<vector<int>>& points, int r) {
-        // 给定两点，dis<2r，那么过这两个点的圆有2个，计算园内的点
-        int n = points.size();
+        // 给定一个点A(x1, y1)，以直径2r扫描（顶点记为C），点B(x2, y2)进入圆的角度angle1，出圆的角度angle2
+        // 时间复杂度O(n^2 lgn) lgn是排序的需要
+        // delta = ∠BAC = arccos(d/2r)
+        // AB与x轴的夹角angle = arctan((y2-y1)/(x2-x1))  函数atan2(y2-y1, x2-x1)不用额外判断x2=x1的情况
+        // 而且atan(y/x)只能返回一个角度值[-pi/2,pi/2]，因此确定点的角度很复杂，要判断在哪个象限，不易于处理旋转的问题
+        int r2 = 2 * r;  //直径
         int res = 1;
-        int r2 = r * r;
-        int dm = 4 * r2;  // 直径^2
-        vector<vector<double>> ct(2, vector<double>(2));   // 两个圆心(x, y)
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int dis = dis2(points[i], points[j]);
-                if(dis > dm) continue;
-                ct = center(points[i], points[j], r2, dis);
-                int now1 = 2, now2 = 2;
-                for(int k = 0; k < n; k++) {
-                    if(k == i || k == j) continue;
-                    if(incirc(points[k], ct[0], r2)) now1++;
-                    if(incirc(points[k], ct[1], r2)) now2++;
-                }
-                res = max(max(res, now1), now2);
+        vector<pair<double, int>> angles;  // 进入(出去)的角度，1(-1)
+        for(auto a: points) {
+            angles.clear();
+            for(auto b: points) {
+                double d = dist(a, b);
+                if((a[0] == b[0] && a[1] == b[1]) || d > r2) continue;
+                double delta = acos(d / r2);  // 一定小于pi/2
+                vector<double> angle = findangle(a, b, delta);
+                angles.push_back({angle[0], 1});  // 进入的角度
+                angles.push_back({angle[1], -1}); // 出去的角度
+            }
+            sort(angles.begin(), angles.end(), cmp());
+            int val = 1;
+            for(auto p: angles) {
+                val += p.second;
+                res = max(res, val);
             }
         }
         return res;
