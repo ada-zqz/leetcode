f33994e5a8dfadc04371cd7eb9946413b563cb8b
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-23 09:30:49 +0800

    直接把K当作进位，而不是先转化为数，再每一位相加
    Runtime: 56 ms, faster than 94.39% of C++ online submissions for Add to Array-Form of Integer.
    Memory Usage: 28.8 MB, less than 11.11% of C++ online submissions for Add to Array-Form of Integer.

--- ds989_AddtoArray-FormofInteger.cpp
+++ ds989_AddtoArray-FormofInteger.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& A, int K) {
+        // 直接把K当作进位，而不是先转化为数，再每一位相加
         if(K == 0) return A;
-        vector<int> k;
-        while(K != 0) {
-            // 34 -> [3, 4]
-            k.push_back(K % 10);
+        vector<int> res;
+        for(int i = A.size() - 1; i >= 0; i--) {
+            int n = A[i] + K;
+            res.push_back(n % 10);
+            K = n / 10;
+        }
+        while(K > 0) {
+            res.push_back(K % 10);
             K /= 10;
         }
-        reverse(k.begin(), k.end());
-        if(A.size() < k.size()) swap(A, k);
-        int n1 = A.size(), n2 = k.size(); //n1>=n2
-        vector<int> res;
-        int carry = 0;  //进位
-        int num;
-        for(int i = 0; i < n1; i++) {
-            if(i < n2) {
-                num = A[n1 - 1 - i] + k[n2 - 1 - i] + carry;
-                carry = num / 10;
-                res.push_back(num % 10);
-            }
-            else {
-                num = A[n1 - 1 - i] + carry;
-                carry = num / 10;
-                res.push_back(num % 10);
-            }
-        }
-        if(carry) res.push_back(carry);
         reverse(res.begin(), res.end());
         return res;
     }
