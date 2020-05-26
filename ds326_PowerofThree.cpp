1ae4b489ceed226d0155514305cb6fd3064ef2a3
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-26 10:48:08 +0800

    只有hash; 比while或者直接vmax % n要慢；加上n>0判断中等速度
    Runtime: 32 ms, faster than 14.85% of C++ online submissions for Power of Three.
    Memory Usage: 6.1 MB, less than 100.00% of C++ online submissions for Power of Three.
    换成return n > 0 && st.find(n) != st.end(); 就变快了
    Runtime: 20 ms, faster than 55.44% of C++ online submissions for Power of Three.
    Memory Usage: 6.1 MB, less than 100.00% of C++ online submissions for Power of Three.

--- ds326_PowerofThree.cpp
+++ ds326_PowerofThree.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        // static int vmax = -1;
-        // if(vmax < 0) {
-        //     vmax = pow(3, (int)(log(0x7fffffff) / log(3)));
-        // }
-        // cout << vmax << endl;
-        // return n > 0 && vmax % n == 0; 
-         return n > 0 && 1162261467 % n == 0; 
+        static unordered_set<int> st;
+        if(st.size() == 0) {
+            long long pre = 1;
+            for(int i = 0; pre <= 0x7fffffff; i++) {
+                st.insert(pre);
+                pre *= 3;
+            }
+        }
+        return st.find(n) != st.end();
     }
 };
