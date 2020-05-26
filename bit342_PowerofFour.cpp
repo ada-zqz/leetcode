11dcb354fc37a6a13c8a765f042af18ab652cb85
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-26 11:11:19 +0800

    先判断是2的指数，(n - 1) % 3 == 0判断是4的指数
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Power of Four.
    Memory Usage: 6.1 MB, less than 100.00% of C++ online submissions for Power of Four.

--- bit342_PowerofFour.cpp
+++ bit342_PowerofFour.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        while(num > 0 && num % 4 == 0) num /= 4;
-        return num == 1;
+        //  4^n - 1 = (2^n + 1) * (2^n - 1)
+        // (2^n + 1) (2^n) (2^n - 1)三个数中一定有个数是3的倍数
+        // 2^n可能是4^n或者2*4^n, 2*4^n - 1 = 2*(4^n - 1)+1，前半部分是3的倍数，加1不是3的倍数
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
     }
 };
