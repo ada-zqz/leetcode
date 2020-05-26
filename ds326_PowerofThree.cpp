fe07d6b08ac152626ffbfb94b8e7422ba40a60b7
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-26 10:37:24 +0800

    static vmax的计算几乎不影响速度， vmax%n占大量时间
    Runtime: 24 ms, faster than 37.24% of C++ online submissions for Power of Three.
    Memory Usage: 6.1 MB, less than 100.00% of C++ online submissions for Power of Three.

--- ds326_PowerofThree.cpp
+++ ds326_PowerofThree.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        static int vmax = -1;
-        if(vmax < 0) {
-            vmax = pow(3, (int)(log(0x7fffffff) / log(3)));
-        }
-        return n > 0 && vmax % n == 0; 
+        // static int vmax = -1;
+        // if(vmax < 0) {
+        //     vmax = pow(3, (int)(log(0x7fffffff) / log(3)));
+        // }
+        // cout << vmax << endl;
+        // return n > 0 && vmax % n == 0; 
+         return n > 0 && 1162261467 % n == 0; 
     }
 };
