906e4f894538dc4aaad1daefe751fc9b4312b1a1
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-27 19:05:20 +0800

    bitset<32>(n).count(); n & 1; n &= n - 1;
    Runtime: 4 ms, faster than 40.96% of C++ online submissions for Number of 1 Bits.
    Memory Usage: 6.3 MB, less than 100.00% of C++ online submissions for Number of 1 Bits.

--- bit191_Numberof1Bits.cpp
+++ bit191_Numberof1Bits.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        // 直接调用bitset函数
         // return bitset<32>(n).count();
         int res = 0;
-        for(int i = 0; i < 32; i++) {
-            res += n & 1;
-            n = n >> 1;
+        // 直接计算每一位是否是1
+        // for(int i = 0; i < 32; i++) {
+        //     res += n & 1;
+        //     n = n >> 1;
+        // }
+        while(n) {
+            n &= n - 1;  // 计算一次少一个1，比上面两种方法多4ms...
+            res++;
         }
         return res;
     }
