cf47c66b65cb53eacf7d54b7c78f416058c7a795
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-27 19:02:02 +0800

    直接数每一位是否为1; res += n & 1; n = n >> 1;
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of 1 Bits.
    Memory Usage: 6.2 MB, less than 100.00% of C++ online submissions for Number of 1 Bits.

--- bit191_Numberof1Bits.cpp
+++ bit191_Numberof1Bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return bitset<32>(n).count();
+        // return bitset<32>(n).count();
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            res += n & 1;
+            n = n >> 1;
+        }
+        return res;
     }
 };
