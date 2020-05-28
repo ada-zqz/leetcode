2fcb861e45bfbb564a8ff4e4590451486b7dfe77
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-28 10:55:06 +0800

    (res << 1) | (n & 1);
    Runtime: 4 ms, faster than 49.29% of C++ online submissions for Reverse Bits.
    Memory Usage: 6.2 MB, less than 100.00% of C++ online submissions for Reverse Bits.

--- bit190_ReverseBits.cpp
+++ bit190_ReverseBits.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        // uint32_t res = 0;
-        // for(int i = 0; i < 32; i++) {
-        //     res = (res << 1) + (n & 1);
-        //     n = n >> 1;
-        // }
-        // return res;
+        uint32_t res = 0;
+        for(int i = 0; i < 32; i++) {
+            // res = (res << 1) + (n & 1);
+            res = (res << 1) | (n & 1);
+            n = n >> 1;
+        }
+        return res;
         
-        // abcd -> cdab -> dcba
-        n = (n >> 16) | (n << 16);
-        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
-        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
-        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2); //1100 - c
-        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1); //1010 - a
-        return n;
+        // // abcd -> cdab -> dcba
+        // n = (n >> 16) | (n << 16);
+        // n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        // n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        // n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2); //1100 - c
+        // n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1); //1010 - a
+        // return n;
     }
 };
