9687aaa34a6c555cc3677071700bb6188f97e924
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-05-29 11:18:47 +0800

    __builtin_popcount(b)
    sort(arr.begin(), arr.end(), [](const int& a, const int& b){int ca = __builtin_popcount(a), cb = __builtin_popcount(b);
    return ca == cb ? a < b : ca < cb;});
    Runtime: 16 ms, faster than 91.44% of C++ online submissions for Sort Integers by The Number of 1 Bits.
    Memory Usage: 10.1 MB, less than 100.00% of C++ online submissions for Sort Integers by The Number of 1 Bits.

--- bit1356_SortIntegersbyTheNumberof1Bits.cpp
+++ bit1356_SortIntegersbyTheNumberof1Bits.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), cmp);
+    //     sort(arr.begin(), arr.end(), cmp);
+    //     return arr;
+    // }
+    // static bool cmp(int& a, int& b) {
+    //     if(bitset<32>(a).count() == bitset<32>(b).count()) return a < b;
+    //     return bitset<32>(a).count() < bitset<32>(b).count();
+
+        sort(arr.begin(), arr.end(), [](const int& a, const int& b){
+            int ca = __builtin_popcount(a), cb = __builtin_popcount(b);
+            return ca == cb ? a < b : ca < cb;});
         return arr;
-    }
-    static bool cmp(int& a, int& b) {
-        if(bitset<32>(a).count() == bitset<32>(b).count()) return a < b;
-        return bitset<32>(a).count() < bitset<32>(b).count();
     }
 };
