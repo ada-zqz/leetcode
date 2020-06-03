dc7d28f0d1619ad557bf88e05fc4fdb7074868f8
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-03 12:22:40 +0800

    二分查找；最小公倍数
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Ugly Number III.
    Memory Usage: 6.1 MB, less than 100.00% of C++ online submissions for Ugly Number III.

--- bs1201_UglyNumberIII.cpp
+++ bs1201_UglyNumberIII.cpp
@@ -1,19 +1,31 @@
-？*
+/*
 1 <= n, a, b, c <= 10^9
 1 <= a * b * c <= 10^18
 */
+typedef long long ll;
 class Solution {
 public:
     int nthUglyNumber(int n, int a, int b, int c) {
-        vector<int> dp(n, 0);
-        int na = 1, nb = 1, nc = 1;
-        for(int i = 0; i < n; i++) {
-            dp[i] = min(na * a, min(nb * b, nc * c));
-            // 相同的乘积由于下面三个判断都++了
-            if(dp[i] == na * a) na++;
-            if(dp[i] == nb * b) nb++;
-            if(dp[i] == nc * c) nc++;
+        ll l = 1, r = 2e9;
+        ll ab = (ll)(a) * (ll)(b) / gcd((ll)(a), (ll)(b));  //最小公倍数
+        ll ac = (ll)(a) * (ll)(c) / gcd((ll)(a), (ll)(c));
+        ll bc = (ll)(b) * (ll)(c) / gcd((ll)(b), (ll)(c));
+        ll abc = (ll)(a) * bc / gcd((ll)(a), (ll)(bc));     //mid是abc的倍数，多减了一次要加回来
+        while(l < r) {
+            ll mid = (l + r) / 2;
+            ll m = mid / a + mid / b + mid / c - mid / (ab) - mid / (ac) - mid / (bc) + mid / (abc);
+            if(m >= n) r = mid;
+            else l = mid + 1;
         }
-        return dp[n - 1];
+        return l;
+    }
+    ll gcd(ll a, ll b) {
+        //a<b
+        //最大公倍数
+        while(a > 0) {
+            b = b % a;
+            swap(a, b);
+        }
+        return b;
     }
 };
