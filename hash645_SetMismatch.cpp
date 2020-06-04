6381aff2cb26df78a6549f5647465fe6b8e02eb6
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-04 11:18:10 +0800

    if(nm[abs(nm[i]) - 1] < 0) 出现两次；res[1] ^= res[0];
    Runtime: 60 ms, faster than 71.35% of C++ online submissions for Set Mismatch.
    Memory Usage: 21.7 MB, less than 25.00% of C++ online submissions for Set Mismatch.

--- hash645_SetMismatch.cpp
+++ hash645_SetMismatch.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        unordered_set<int> st;
-        vector<int> res;
-        for(int i: nums) {
-            if(st.find(i) == st.end()) st.insert(i);
-            else res.push_back(i);
-        }
-        for(int i = 1; i <= nums.size(); i++) {
-            if(st.find(i) == st.end()) {
-                res.push_back(i);
-                break;
+        vector<int> res(2, 0);
+        for(int i = 0; i < nums.size(); i++) {
+            res[1] ^= (i + 1) ^ abs(nums[i]);
+            if(nums[abs(nums[i]) - 1] < 0) {
+                // 出现两次
+                res[0] = abs(nums[i]);
+            }
+            else {
+                // idx处设置为负数
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
             }
         }
+        // res[1] = 重复的数^少的数
+        res[1] ^= res[0];
         return res;
     }
 };
