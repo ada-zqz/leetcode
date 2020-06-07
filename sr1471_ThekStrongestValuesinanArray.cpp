46b86310524d296bd85cd4b41ae164467ad222b5
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-07 15:06:21 +0800

    multiset<pair<int, int>, decltype(cmp)> mt(cmp);
    理解错题意：除了val，还考虑idx的大小
    auto cmp = [](pair<int, int> u1, pair<int, int> u2) { 
                if(u1.first != u2.first) return u1.first > u2.first; 
                return u1.second < u2.second;     
            };
     multiset<pair<int, int>, decltype(cmp)> mt(cmp);  //val, idx

--- sr1471_ThekStrongestValuesinanArray.cpp
+++ sr1471_ThekStrongestValuesinanArray.cpp
@@ -3,21 +3,26 @@ public:
     vector<int> getStrongest(vector<int>& arr, int k) {
         int n = arr.size();
         if(n == 1) return vector<int> {arr[0]};
+        vector<int> brr;
+        for(auto a: arr) brr.push_back(a);
         sort(arr.begin(), arr.end());
         int mn = (n - 1) / 2;
         int mid = arr[mn];
-        vector<int> res;
-        auto cmp = [](pair<int, int> u1, pair<int, int> u2) { return u1.first < u2.first; };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-        pq.push({abs(arr[0] - mid), 0});
-        pq.push({abs(arr[n - 1] - mid), n - 1});
-        while(res.size() < k) {
-            auto p = pq.top();
-            pq.pop();
-            cout<<p.second<<" ";
-            res.push_back(arr[p.second]);
-            if(p.second > mn) pq.push({abs(arr[p.second - 1] - mid), p.second - 1});
-            if(p.second < mn) pq.push({abs(arr[p.second + 1] - mid), p.second + 1});
+        auto cmp = [](pair<int, int> u1, pair<int, int> u2) { 
+            if(u1.first != u2.first) return u1.first > u2.first; 
+            return u1.second < u2.second;     
+        };
+        multiset<pair<int, int>, decltype(cmp)> mt(cmp);  //val, idx
+        for(int i = 0; i < n; i++) {
+            mt.insert({abs(brr[i] - mid), i});
+        }
+        
+        vector<int> res(k);
+        int i = 0;
+        for(auto m: mt) {
+            if(i < k) {
+                res[i++] = brr[m.second];
+            }
         }
         return res;
     }
