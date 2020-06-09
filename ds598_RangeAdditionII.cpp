fd0a6e6c64b719abee39ea69274166ab6253ac11
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-09 11:05:05 +0800

    用if而不是min快一点
    Runtime: 24 ms, faster than 35.52% of C++ online submissions for Range Addition II.
    Memory Usage: 11.7 MB, less than 7.35% of C++ online submissions for Range Addition II.

--- ds598_RangeAdditionII.cpp
+++ ds598_RangeAdditionII.cpp
@@ -3,8 +3,8 @@ public:
     int maxCount(int m, int n, vector<vector<int>>& ops) {
         int minm = m, minn = n;
         for(auto o: ops) {
-            minm = min(minm, o[0]);
-            minn = min(minn, o[1]);
+            if(minm > o[0]) minm = o[0];
+            if(minn > o[1]) minn = o[1];
         }
         return minm * minn;
     }
