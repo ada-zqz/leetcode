e67a4f725435bb0cedd01f09a62cd686775bc548
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-11 11:31:37 +0800

    do{cur=(cur+k)%n;}while(cur!=start); start++;// 嵌套
    Runtime: 8 ms, faster than 86.93% of C++ online submissions for Rotate Array.
    Memory Usage: 10.1 MB, less than 22.87% of C++ online submissions for Rotate Array.

--- ds189_RotateArray.cpp
+++ ds189_RotateArray.cpp
@@ -4,15 +4,19 @@ public:
         if(k == 0) return;
         
         int n = nums.size();
-        int mv = 0;
-        while(mv++ < k) {
-            int pre = nums[0];
-            for(int i = 1; i < n; i++) {
-                int tmp = nums[i];
-                nums[i] = pre;
+        int start = 0, cur = 0, mv = 0;
+        int pre = nums[0];
+        while(mv < n) {
+            do{
+                int tmp = nums[(cur + k) % n]; //nums[i]交换到nums[(i + k) % n]
+                nums[(cur + k) % n] = pre;
                 pre = tmp;
-            }
-            nums[0] = pre;
+                mv++;
+                cur = (cur + k) % n;
+            }while(cur != start); // 嵌套了 [-1,99,3,100] 2
+            start++;
+            cur = start;
+            pre = nums[cur];
         }
     }
 };
