8cb1537bf27d7f5b94da823c1e7c8f14b801c71f
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-11 12:01:09 +0800

    reverse(nums.begin(), nums.begin() + (n - k));
    Runtime: 8 ms, faster than 86.93% of C++ online submissions for Rotate Array.
    Memory Usage: 10 MB, less than 35.63% of C++ online submissions for Rotate Array.

--- ds189_RotateArray.cpp
+++ ds189_RotateArray.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k == 0) return;
-        
         int n = nums.size();
-        int start = 0, cur = 0, mv = 0;
-        int pre = nums[0];
-        while(mv < n) {
-            do{
-                int tmp = nums[(cur + k) % n]; //nums[i]交换到nums[(i + k) % n]
-                nums[(cur + k) % n] = pre;
-                pre = tmp;
-                mv++;
-                cur = (cur + k) % n;
-            }while(cur != start); // 嵌套了 [-1,99,3,100] 2
-            start++;
-            cur = start;
-            pre = nums[cur];
-        }
+        if(n == 1 || k == 0) return;
+        
+        k = k % n;
+        reverse(nums.begin(), nums.begin() + (n - k));
+        reverse(nums.begin() + (n - k), nums.end());
+        reverse(nums.begin(), nums.end());
     }
 };
