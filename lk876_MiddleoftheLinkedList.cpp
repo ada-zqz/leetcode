54a1b68ca247401da6c43520485c14e9be09c19f
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-13 11:58:30 +0800

    Create lk876_MiddleoftheLinkedList.cpp
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Middle of the Linked List.
    Memory Usage: 6.7 MB, less than 19.87% of C++ online submissions for Middle of the Linked List.

---
+++ lk876_MiddleoftheLinkedList.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        auto fast = head;
+        while(fast && fast->next) {
+            head = head->next;
+            fast = fast->next->next;
+        }
+        return head;
+    }
+};
