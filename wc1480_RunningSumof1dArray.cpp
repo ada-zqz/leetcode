9185ad524f16cc7177a862825c74304461d3adf5
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-15 14:14:52 +0800

    Create wc1480_RunningSumof1dArray.cpp

---
+++ wc1480_RunningSumof1dArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        res[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            res[i] = res[i - 1] + nums[i];
+        }
+        return res;
+    }
+};
