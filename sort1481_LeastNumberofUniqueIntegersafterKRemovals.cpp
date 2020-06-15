bdbaabfc2faf18680a68eb3d4745aba6cd89b358
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-15 14:24:17 +0800

    if(k == 0 || tmp[i] > k) return res;
    Runtime: 604 ms, faster than 72.96% of C++ online submissions for Least Number of Unique Integers after K Removals.
    Memory Usage: 62.3 MB, less than 80.00% of C++ online submissions for Least Number of Unique Integers after K Removals.

--- sort1481_LeastNumberofUniqueIntegersafterKRemovals.cpp
+++ sort1481_LeastNumberofUniqueIntegersafterKRemovals.cpp
@@ -2,36 +2,22 @@ class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         // int n = arr.size();
-        unordered_map<int, int> mp0; //nums, times
+        unordered_map<int, int> mp; //nums, times
         for(auto a: arr) {
-            mp0[a]++;
+            mp[a]++;
         }
-        unordered_map<int, vector<int>> mp;
-        for(auto m: mp0) {
-            mp[m.second].push_back(m.first);  //times, nums
-        }
-        vector<pair<int, vector<int>>> tmp;
-        for (auto& i : mp) tmp.push_back(i);
+        vector<int> tmp; //每个数出现的次数
+        for (auto& m : mp) tmp.push_back(m.second);
         
-        int res = 0;
-        sort(tmp.begin(), tmp.end(), [](pair<int, vector<int>>& a, pair<int, vector<int>>& b) {return a.first < b.first;});
+        int res = tmp.size(); //不同的数的个数
+        sort(tmp.begin(), tmp.end());
         for(int i = 0; i < tmp.size(); i++) {
-            int n = tmp[i].second.size();
-            int t = tmp[i].first;
-            // cout << tmp[i].first << "  --  " <<n << " " << k << endl;
-            if(k > 0) {
-                if(n * t >= k) {
-                    n -= k / t;
-                    k = 0;
-                }
-                else {
-                    //n * t < k
-                    k = k - n * t;
-                    n = 0;
-                }
+            if(k == 0 || tmp[i] > k) {
+                // 到第i个数就能搞定
+                return res;
             }
-            res += n;
-            // cout << n << " " << k << endl;
+            k -= tmp[i];
+            res--;
         }
         return res;
     }
