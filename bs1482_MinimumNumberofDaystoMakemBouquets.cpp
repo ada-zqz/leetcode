09ff91991223fe1b3fc0a3f559d3844dc7c09b6e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-16 19:54:55 +0800

    minmax_element(by.begin(), by.end());
    Runtime: 400 ms, faster than 77.45% of C++ online submissions for Minimum Number of Days to Make m Bouquets.
    Memory Usage: 63.5 MB, less than 10.00% of C++ online submissions for Minimum Number of Days to Make m Bouquets.

---
+++ bs1482_MinimumNumberofDaystoMakemBouquets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minDays(vector<int>& by, int m, int k) {
+        // 二分法：先给定day，判断合不合适，确定上限，然后不断缩小限制
+        int n = by.size();
+        if(n < m * k) return -1;
+        
+        auto v = minmax_element(by.begin(), by.end());
+        int l = *v.first, r = *v.second;
+        int ok;
+        while(l < r) {
+            int mid = (l + r) >> 1;
+            ok = okm(by, mid, k);
+            if(ok >= m) r = mid;
+            else l = mid + 1;
+        }
+        return okm(by, l, k) >= m ? l : -1;
+    }
+    int okm(const vector<int>& by, int & mid, int & k) {
+        int res = 0, cnt = 0;
+        for(auto & b: by) {
+            if(b <= mid) cnt++;
+            else cnt = 0;
+            if(cnt == k) {
+                cnt = 0;
+                res++;
+            }
+        }
+        return res;
+    }
+};
