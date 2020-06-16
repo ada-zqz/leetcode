bdbdf62a16db03e41a68cfb3640bc6751248017b
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-16 20:01:48 +0800

    minmax_element(by.begin(), by.end());

--- bs1482_MinimumNumberofDaystoMakemBouquets.cpp
+++ bs1482_MinimumNumberofDaystoMakemBouquets.cpp
@@ -14,7 +14,8 @@ public:
             if(ok >= m) r = mid;
             else l = mid + 1;
         }
-        return okm(by, l, k) >= m ? l : -1;
+//         return okm(by, l, k) >= m ? l : -1;
+        return l; //第6行已经判断一定可行，只是时间长短的问题
     }
     int okm(const vector<int>& by, int & mid, int & k) {
         int res = 0, cnt = 0;
