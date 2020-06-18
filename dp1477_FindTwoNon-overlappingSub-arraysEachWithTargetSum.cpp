284c7bf914bb3a128a4f55f58bd186dfc485e51e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-18 11:25:57 +0800

    if(left < MAX && mp.find(sm + target) != mp.end())
    Runtime: 640 ms, faster than 53.90% of C++ online submissions for Find Two Non-overlapping Sub-arrays Each With Target Sum.
    Memory Usage: 115 MB, less than 20.00% of C++ online submissions for Find Two Non-overlapping Sub-arrays Each With Target Sum.

--- dp1477_FindTwoNon-overlappingSub-arraysEachWithTargetSum.cpp
+++ dp1477_FindTwoNon-overlappingSub-arraysEachWithTargetSum.cpp
@@ -2,31 +2,28 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
-        vector<int> dp1 = findsub(arr, target); //0-i的子序列中的最短长度
-        reverse(arr.begin(), arr.end());
-        vector<int> dp2 = findsub(arr, target); //序列倒置后0-i的子序列中的最短长度
+        unordered_map<int, int> mp; //sm, index
+        int sm = 0;
+        for(int i = 0; i < n; i++) {
+            sm += arr[i];
+            // sm单调递增的，不用担心有相同的sm对应不同的i
+            mp[sm] = i;
+        }
         
-        int vmin = 0x3f3f3f;
+        int vmin = 0x3f3f3f, left = 0x3f3f3f;
+        sm = 0;
+        mp[0] = -1;
         for(int i = 0; i < n - 1; i++) {
-            vmin = min(vmin, dp1[i] + dp2[n - i - 2]); //不overlap的两个子序列
+            sm += arr[i];
+            if(mp.find(sm - target) != mp.end()) {
+                // 0-i: sm[i]; i+1-j:target; 0-j:sm[j]
+                left = min(left, i - mp[sm - target]);
+            }
+            if(left < 0x3f3f3f && mp.find(sm + target) != mp.end()) {
+                //左边的子序列找到，找右边的子序列
+                vmin = min(vmin, left + mp[sm + target] - i);
+            }
         }
         return vmin >= 0x3f3f3f ? -1 : vmin;
-    }
-    vector<int> findsub(vector<int>& v, int & t) {
-        int n = v.size();
-        vector<int> dp(n, 0x3f3f3f);
-        vector<int> sm(n, 0);
-        unordered_map<int, int> mp; //sm, index
-        mp[0] = -1;  //用于sm[i] == target
-        for(int i = 0; i < n; i++) {
-            sm[i] = i == 0 ? v[i] : sm[i - 1] + v[i];
-            if(mp.find(sm[i] - t) != mp.end()) {
-                // 0-i: sm[i]; i+1-j:target; 0-j:sm[j]
-                dp[i] = min(dp[i], i - mp[sm[i] - t]);
-            }
-            mp[sm[i]] = i; //之前有一样的和sm[i]，也更新为i，求最短距离
-            if(i > 0) dp[i] = min(dp[i], dp[i - 1]);
-        }
-        return dp;
     }
 };
