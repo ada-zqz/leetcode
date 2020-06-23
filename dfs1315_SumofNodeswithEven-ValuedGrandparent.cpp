35f6fc4196197fa90dee81468c7c10b8a607ece9
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-23 16:10:18 +0800

    记录parent和grandparent，递归
    Runtime: 60 ms, faster than 71.40% of C++ online submissions for Sum of Nodes with Even-Valued Grandparent.
    Memory Usage: 38.2 MB, less than 64.85% of C++ online submissions for Sum of Nodes with Even-Valued Grandparent.

--- dfs1315_SumofNodeswithEven-ValuedGrandparent.cpp
+++ dfs1315_SumofNodeswithEven-ValuedGrandparent.cpp
@@ -11,24 +11,8 @@
  */
 class Solution {
 public:
-    int sumEvenGrandparent(TreeNode* root) {
-        int res = 0;
-        dfs(root, res);
-        return res;
-    }
-    void dfs(TreeNode* root, int & res) {
-        if(!root) return;
-        if(root->val % 2 == 0) {
-            if(root->left) {
-                if(root->left->left) res += root->left->left->val;
-                if(root->left->right) res += root->left->right->val;
-            }
-            if(root->right) {
-                if(root->right->left) res += root->right->left->val;
-                if(root->right->right) res += root->right->right->val;
-            }
-        }
-        dfs(root->left, res);
-        dfs(root->right, res);
+    int sumEvenGrandparent(TreeNode* root, int p = 1, int gp = 1) {
+        return root ? sumEvenGrandparent(root->left, root->val, p) + 
+           sumEvenGrandparent(root->right, root->val, p) + (gp % 2 ? 0 : root->val) : 0;
     }
 };
