f96ac33c002619df25997b3ee9d367397ca69c66
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-28 16:33:49 +0800

    Create wc1493_LongestSubarrayof1'sAfterDeletingOneElement.cpp

---
+++ wc1493_LongestSubarrayof1'sAfterDeletingOneElement.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return 0;
+        
+        int i = 0;
+        while(i < n && nums[i] == 0) i++;
+        if(i == n) return 0;
+        
+        
+        int res = 1, pre1 = 1, pre2 = 1;
+        int meet = 0;
+        for(i = ++i; i < n; i++) {
+            if(nums[i]) {
+                if(meet == 0) {
+                    //没有遇到0
+                    pre1++;
+                    pre2++;
+                    res = max(res, pre1);
+                }
+                else {
+                    // 前面有个0
+                    pre2++;
+                    pre1++;
+                    res = max(res, pre2);
+                }
+            }
+            else {
+                //遇到0
+                if(meet == 0) {
+                    meet = 1;
+                    pre1 = 0;
+                }
+                else {
+                    pre2= pre1;
+                    pre1 = 0;
+                }
+            }
+        }
+        if(nums[0] == 1 && meet == 0) res--;
+        
+        return res;
+    }
+};
