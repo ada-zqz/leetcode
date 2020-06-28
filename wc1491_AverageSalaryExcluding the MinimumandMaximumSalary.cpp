74978a167d14f62a5855df39d89775b976791b0e
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-28 16:22:06 +0800

    accumulate(begin(s), end(s), 0);minmax_element();
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Average Salary Excluding the Minimum and Maximum Salary.
    Memory Usage: 7.3 MB, less than 33.33% of C++ online submissions for Average Salary Excluding the Minimum and Maximum Salary.

--- wc1491_AverageSalaryExcluding the MinimumandMaximumSalary.cpp
+++ wc1491_AverageSalaryExcluding the MinimumandMaximumSalary.cpp
@@ -4,7 +4,8 @@ public:
         auto m = minmax_element(s.begin(), s.end()); 
         double res = 0;
         int n = s.size();
-        for(auto i: s) {res += i;}
+        // for(auto i: s) {res += i;}
+        res = accumulate(begin(s), end(s), 0);
         res -= *m.first;
         res -= *m.second;
         res = res / (n - 2);
