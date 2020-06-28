ec5fd30d0294d75acdb3bec2a45c9c98b586b2b5
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-28 16:31:40 +0800

    if (d * d != n && n % d == 0 && --k == 0)
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for The kth Factor of n.
    Memory Usage: 6.1 MB, less than 33.33% of C++ online submissions for The kth Factor of n.

--- wc1492_ThekthFactorofn.cpp
+++ wc1492_ThekthFactorofn.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        int l = 0;
-        for(int i = 1; i <= n; i++) {
-            if(n % i == 0) l++;
-            if(l == k) return i;
+        int d = 1;
+        for (; d * d <= n; ++d)
+            // 也有牺牲空间，在这一步就把n/k存到数组中，之后直接取出来的
+            if (n % d == 0 && --k == 0)
+                return d;
+        for (d = d - 1; d >= 1; --d) {
+            if (d * d != n && n % d == 0 && --k == 0)
+                return n / d;
         }
         return -1;
     }
