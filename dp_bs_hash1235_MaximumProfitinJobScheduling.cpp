80ad580e201f8daa8a1e58558e87ae5ddf1be040
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-06-29 12:46:27 +0800

    prev(dp.upper_bound(j[1]))->second + j[2];
    dp用map<int, int>存储，upper_bound查找（binary search）
    vector<vector<int>> 可以sort
    
    Runtime: 544 ms, faster than 22.01% of C++ online submissions for Maximum Profit in Job Scheduling.
    Memory Usage: 63.3 MB, less than 25.39% of C++ online submissions for Maximum Profit in Job Scheduling.

---
+++ dp_bs_hash1235_MaximumProfitinJobScheduling.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<vector<int>> jb(n);
+        for(int i = 0; i < n; i++) {
+            jb[i] = vector<int>{endTime[i], startTime[i], profit[i]};
+        }
+        // 根据endtime从小到大排序
+        sort(jb.begin(), jb.end());
+        
+        // dp + binary search
+        map<int, int> dp = {{0, 0}}; //endtime, total profit
+        for(auto j: jb) {
+            
+            // // int cur = dp.lower_bound(j[1])->second + j[2]; 计算错误，找到的位置不对
+            // // [1,2,3,4,6]
+            // // [3,5,10,6,9]
+            // // [20,20,100,70,60]
+            // for(auto d: dp) cout << d.first << ":" << d.second << " ";
+            // cout << endl;
+            // cout << j[0] << " start: " << j[1] << " find: " << dp.lower_bound(j[1])->first << endl;
+            // cout << j[0] << " start: " << j[1] << " find: " << prev(dp.upper_bound(j[1]))->first << endl;
+            
+            // starttime>j[1]往前走一个的profit + 此时的profit:j[2]
+            int cur = prev(dp.upper_bound(j[1]))->second + j[2];
+            if(cur > dp.rbegin()->second) dp[j[0]] = cur;
+        }
+        return dp.rbegin()->second;
+    }
+};
