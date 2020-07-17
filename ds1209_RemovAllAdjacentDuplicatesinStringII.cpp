49313e4509abaa28258c16664b8b45cf574b80fb
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-07-17 10:59:23 +0800

    if(++st.back().first == k) st.pop_back();
    Runtime: 36 ms, faster than 57.25% of C++ online submissions for Remove All Adjacent Duplicates in String II.
    Memory Usage: 8.2 MB, less than 72.40% of C++ online submissions for Remove All Adjacent Duplicates in String II.

--- ds1209_RemovAllAdjacentDuplicatesinStringII.cpp
+++ ds1209_RemovAllAdjacentDuplicatesinStringII.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        int i = 0, n = s.size();
-        // 字符连续出现几次
-        vector<int> count(n);
-        for(int j = 0; j < n; j++, i++) {
-            s[i] = s[j];
-            count[i] = i > 0 && s[i - 1] == s[j] ? count[i - 1] + 1 : 1;
-            if(count[i] == k) i -= k;
+        vector<pair<int, char>> stack = {{0, '#'}}; //简化下面的判断表达式
+        for(char c: s) {
+            if(stack.back().second != c) {
+                stack.push_back({1, c});
+            } 
+            else if(++stack.back().first == k)
+                stack.pop_back();
         }
-        return s.substr(0, i);
+        string res;
+        for(auto& r: stack) {
+            res.append(r.first, r.second);
+        }
+        return res;
     }
 };
