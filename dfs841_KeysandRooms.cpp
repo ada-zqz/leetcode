fbd96ca919923ba0a4e519a70ccccc7b7afedc30
ada-zqz <40237587+ada-zqz@users.noreply.github.com>
2020-07-26 12:59:01 +0800

    BFS; queue<int> to_visit;
    Runtime: 16 ms, faster than 94.64% of C++ online submissions for Keys and Rooms.
    Memory Usage: 10.9 MB, less than 30.09% of C++ online submissions for Keys and Rooms.

--- dfs841_KeysandRooms.cpp
+++ dfs841_KeysandRooms.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        // BFS
         unordered_set<int> visited;
-        dfs(rooms, visited, 0);
+        queue<int> to_visit;
+        to_visit.push(0);
+        while(!to_visit.empty()) {
+            int cur = to_visit.front();
+            visited.insert(cur);
+            to_visit.pop();
+            for(int k : rooms[cur]) 
+                if (visited.find(k) == visited.end()) to_visit.push(k);
+        }
         return visited.size() == rooms.size();
     }
-    void dfs(vector<vector<int>>& rooms, unordered_set<int> & visited, int cur) {
-        visited.insert(cur);
-        for(int k : rooms[cur]) {
-            if(visited.find(k) == visited.end()) 
-                dfs(rooms, visited, k);
-        }
-    }
 };
-
-    
-    
